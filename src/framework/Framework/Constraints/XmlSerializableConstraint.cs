4d4375f5c2ea6d242e00832531021a4af5748028
Charlie Poole <charlie@nunit.com>
2009-09-03 12:37:05 -0700

    Split BinarySerializableConstraint and XmlSerializableConstraint into separate files
    --HG--
    branch : nunitlite
    extra : convert_revision : charlie%40nunit.com-20090903193705-x3804fmx2pqpudn1

--- src/framework/Framework/Constraints/XmlSerializableConstraint.cs
+++ src/framework/Framework/Constraints/XmlSerializableConstraint.cs
@@ -21,93 +21,14 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // ***********************************************************************
 
+#if !NETCF_1_0
 using System;
 using System.IO;
-#if !NETCF
 using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
-#endif
 using System.Xml.Serialization;
 
 namespace NUnit.Framework.Constraints
 {
-#if !NETCF
-    #region BinarySerializableConstraint
-
-    /// <summary>
-    /// BinarySerializableConstraint tests whether 
-    /// an object is serializable in binary format.
-    /// </summary>
-    public class BinarySerializableConstraint : Constraint
-    {
-        readonly BinaryFormatter serializer = new BinaryFormatter();
-
-        /// <summary>
-        /// Test whether the constraint is satisfied by a given value
-        /// </summary>
-        /// <param name="actual">The value to be tested</param>
-        /// <returns>True for success, false for failure</returns>
-        public override bool Matches(object actual)
-        {
-            this.actual = actual;
-
-            if(actual == null)
-                throw new ArgumentException();
-
-            MemoryStream stream = new MemoryStream();
-
-            try
-            {
-                serializer.Serialize(stream, actual);
-
-                stream.Seek(0, SeekOrigin.Begin);
-
-                object value = serializer.Deserialize(stream);
-
-                return value != null;
-            }
-            catch (SerializationException)
-            {
-                return false;
-            }
-        }
-
-        /// <summary>
-        /// Write the constraint description to a MessageWriter
-        /// </summary>
-        /// <param name="writer">The writer on which the description is displayed</param>
-        public override void WriteDescriptionTo(MessageWriter writer)
-        {
-            writer.Write("binary serializable");
-        }
-
-        /// <summary>
-        /// Write the actual value for a failing constraint test to a
-        /// MessageWriter. The default implementation simply writes
-        /// the raw value of actual, leaving it to the writer to
-        /// perform any formatting.
-        /// </summary>
-        /// <param name="writer">The writer on which the actual value is displayed</param>
-        public override void WriteActualValueTo(MessageWriter writer)
-        {
-            writer.Write("<{0}>", actual.GetType().Name);
-        }
-
-        /// <summary>
-        /// Returns the string representation
-        /// </summary>
-        public override string ToString()
-        {
-            return "<binaryserializable>";
-        }
-    }
-
-    #endregion
-#endif
-
-#if !NETCF_1_0
-    #region XmlSerializableConstraint
-
     /// <summary>
     /// BinarySerializableConstraint tests whether 
     /// an object is serializable in binary format.
@@ -181,7 +102,5 @@ namespace NUnit.Framework.Constraints
             return "<xmlserializable>";
         }
     }
-
-    #endregion
-#endif
 }
+#endif
