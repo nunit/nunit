dd66bf6cd42cbcdf04aff62244dd2de80235a735
Charlie Poole <charlie@nunit.com>
2009-09-17 13:45:00 -0700

    Change comment in generated code
    --HG--
    branch : nunitlite
    extra : convert_revision : charlie%40nunit.com-20090917204500-phsz5ztkkwhxav8e

--- src/framework/Shared/Framework/Constraints/ConstraintFactory.cs
+++ src/framework/Shared/Framework/Constraints/ConstraintFactory.cs
@@ -697,7 +697,7 @@ namespace NUnit.Framework.Constraints
 #if !NETCF
         /// <summary>
         /// Returns a constraint that succeeds if the actual
-        /// value matches the Regex pattern supplied as an argument.
+        /// value matches the regular expression supplied as an argument.
         /// </summary>
         public RegexConstraint Matches(string pattern)
         {
@@ -706,7 +706,7 @@ namespace NUnit.Framework.Constraints
 
         /// <summary>
         /// Returns a constraint that succeeds if the actual
-        /// value matches the Regex pattern supplied as an argument.
+        /// value matches the regular expression supplied as an argument.
         /// </summary>
         public RegexConstraint StringMatching(string pattern)
         {
