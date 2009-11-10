038cde3a61057d7ae13d1339c1e132afbe62c2f4
Charlie Poole <charlie@nunit.com>
2009-11-09 16:50:25 -0800

    Add missing files and rename file to match class name
    --HG--
    rename : src/framework/NUnit/Core/NullListener.cs => src/framework/NUnit/Core/TestListener.cs
    extra : convert_revision : charlie%40nunit.com-20091110005025-0fmy9c5ykh5hnk32

---
+++ src/framework/NUnit/Core/TestDriver.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using NUnit.Core.Builders;
+
+namespace NUnit.Core
+{
+    public class TestDriver : RemoteTestRunner
+    {
+        #region TestControllerHelper Classes
+
+        /// <summary>
+        /// TestControllerHelper is the base class for all helpers
+        /// used to invoke methods on TestDriver. It provides
+        /// the callback for state reporting.
+        /// </summary>
+        public abstract class TestControllerHelper : MarshalByRefObject
+        {
+            private AsyncCallback callback;
+
+            protected TestControllerHelper(AsyncCallback callback)
+            {
+                this.callback = callback;
+            }
+
+            public override object InitializeLifetimeService()
+            {
+                return null;
+            }
+
+            public void Report(object result, bool isCompleted)
+            {
+                callback(new TestDriverResult(result, isCompleted));
+            }
+        }
+
+        public class LoadTests : TestControllerHelper
+        {
+            public LoadTests(TestDriver driver, string assemblyFilename, AsyncCallback callback) : base(callback)
+            {
+                Report( driver.Load(new TestPackage(assemblyFilename)), true );
+            }
+        }
+
+        public class CountTests : TestControllerHelper
+        {
+            public CountTests(TestDriver driver, AsyncCallback callback) : base(callback)
+            {
+                Report(driver.CountTestCases(TestFilter.Empty), true);
+            }
+        }
+
+        public class RunTests : TestControllerHelper, ITestListener
+        {
+            public RunTests(TestDriver driver, AsyncCallback callback)
+                : base(callback)
+            {
+                Report(driver.Run(this, TestFilter.Empty), true);
+            }
+
+            #region ITestListener Members
+
+            public void RunStarted(TestName testName, int testCount)
+            {
+            }
+
+            public void RunFinished(TestResult result)
+            {
+            }
+
+            public void RunFinished(Exception exception)
+            {
+            }
+
+            public void TestStarted(TestName testName)
+            {
+            }
+
+            public void TestFinished(TestResult result)
+            {
+            }
+
+            public void SuiteStarted(TestName testName)
+            {
+            }
+
+            public void SuiteFinished(TestResult result)
+            {
+            }
+
+            public void UnhandledException(Exception exception)
+            {
+            }
+
+            public void TestOutput(TestOutput testOutput)
+            {
+                Report(testOutput, false);
+            }
+
+            #endregion
+        }
+
+        #endregion
+
+        [Serializable]
+        public class TestDriverResult : IAsyncResult
+        {
+            private object state;
+            private bool isCompleted;
+
+            public TestDriverResult(object state, bool isCompleted)
+            {
+                this.state = state;
+                this.isCompleted = isCompleted;
+            }
+
+            #region IAsyncResult Members
+
+            public object AsyncState
+            {
+                get { return state; }
+            }
+
+            public System.Threading.WaitHandle AsyncWaitHandle
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public bool CompletedSynchronously
+            {
+                get { return false; }
+            }
+
+            public bool IsCompleted
+            {
+                get { return isCompleted; }
+            }
+
+            #endregion
+        }
+    }
+}
