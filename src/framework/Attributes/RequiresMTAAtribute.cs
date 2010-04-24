b4377c3ea0c89e63ab5b09211f5a7359b02b7902
Charlie Poole <charlie@nunit.com>
2010-04-24 10:46:02 -0700

    Split all attributes into separate files
    --HG--
    rename : src/framework/Attributes/JoinTypeAttributes.cs => src/framework/Attributes/CombinatorialAttribute.cs
    rename : src/framework/Attributes/IncludeExcludeAttributes.cs => src/framework/Attributes/IncludeExcludeAttribute.cs
    rename : src/framework/Attributes/ThreadingAttributes.cs => src/framework/Attributes/RequiresThreadAttribute.cs
    extra : convert_revision : charlie%40nunit.com-20100424174602-xs3z7gs8qpy2yflv

---
+++ src/framework/Attributes/RequiresMTAAtribute.cs
@@ -0,0 +1,49 @@
+﻿// ***********************************************************************
+// Copyright (c) 2008 Charlie Poole
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// ***********************************************************************
+
+#if !NUNITLITE
+using System;
+using System.Threading;
+
+namespace NUnit.Framework
+{
+    /// <summary>
+    /// Marks a test that must run in the MTA, causing it
+    /// to run in a separate thread if necessary.
+    /// 
+    /// On methods, you may also use MTAThreadAttribute
+    /// to serve the same purpose.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Assembly, AllowMultiple = false)]
+    public class RequiresMTAAttribute : PropertyAttribute
+    {
+        /// <summary>
+        /// Construct a RequiresMTAAttribute
+        /// </summary>
+        public RequiresMTAAttribute()
+        {
+            this.Properties.Add("APARTMENT_STATE", ApartmentState.MTA);
+        }
+    }
+}
+#endif
