f7520544e140a9d1e8a75d8ecf23f1865319dbb8
Charlie Poole <charlie@nunit.com>
2010-07-17 06:44:46 -0700

    Remove DirectoryAssert and SubDirectoryConstraint from build
    --HG--
    rename : src/framework/Constraints/DirectoryConstraints.cs => src/framework/Constraints/EmptyDirectoryConstraint.cs
    extra : convert_revision : charlie%40nunit.com-20100717134446-di2n41zbnns1yh5j

--- src/framework/Constraints/EmptyDirectoryConstraint.cs
+++ src/framework/Constraints/EmptyDirectoryConstraint.cs
@@ -1,5 +1,5 @@
 // ***********************************************************************
-// Copyright (c) 2007 Charlie Poole
+// Copyright (c) 2008 Charlie Poole
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
@@ -22,49 +22,63 @@
 // ***********************************************************************
 
 using System;
-using System.Collections;
+using System.IO;
 
 namespace NUnit.Framework.Constraints
 {
-	/// <summary>
-	/// EmptyConstraint tests a whether a string or collection is empty,
-	/// postponing the decision about which test is applied until the
-	/// type of the actual argument is known.
-	/// </summary>
-	public class EmptyConstraint : Constraint
-	{
-		private Constraint RealConstraint
-		{
-			get 
-			{
-				if ( actual is string )
-					return new EmptyStringConstraint();
-                else if (actual is System.IO.DirectoryInfo)
-                    return new EmptyDirectoryContraint();
-                else
-					return new EmptyCollectionConstraint();
-			}
-		}
-		
-		/// <summary>
+    /// <summary>
+    /// EmptyDirectoryConstraint is used to test that a directory is empty
+    /// </summary>
+    public class EmptyDirectoryContraint : Constraint
+    {
+        private int files = 0;
+        private int subdirs = 0;
+
+        /// <summary>
         /// Test whether the constraint is satisfied by a given value
         /// </summary>
         /// <param name="actual">The value to be tested</param>
         /// <returns>True for success, false for failure</returns>
-		public override bool Matches(object actual)
-		{
-			this.actual = actual;
+        public override bool Matches(object actual)
+        {
+            this.actual = actual;
 
-			return this.RealConstraint.Matches( actual );
-		}
+            DirectoryInfo dirInfo = actual as DirectoryInfo;
+            if (dirInfo == null)
+                throw new ArgumentException("The actual value must be a DirectoryInfo", "actual");
+
+            files = dirInfo.GetFiles().Length;
+            subdirs = dirInfo.GetDirectories().Length;
+
+            return files == 0 && subdirs == 0;
+        }
 
         /// <summary>
         /// Write the constraint description to a MessageWriter
         /// </summary>
         /// <param name="writer">The writer on which the description is displayed</param>
-		public override void WriteDescriptionTo(MessageWriter writer)
-		{
-			this.RealConstraint.WriteDescriptionTo( writer );
-		}
-	}
+        public override void WriteDescriptionTo(MessageWriter writer)
+        {
+            writer.Write( "An empty directory" );
+        }
+
+        /// <summary>
+        /// Write the actual value for a failing constraint test to a
+        /// MessageWriter. The default implementation simply writes
+        /// the raw value of actual, leaving it to the writer to
+        /// perform any formatting.
+        /// </summary>
+        /// <param name="writer">The writer on which the actual value is displayed</param>
+        public override void WriteActualValueTo(MessageWriter writer)
+        {
+            DirectoryInfo dir = actual as DirectoryInfo;
+            if (dir == null)
+                base.WriteActualValueTo(writer);
+            else
+            {
+                writer.WriteActualValue(dir);
+                writer.Write(" with {0} files and {1} directories", files, subdirs);
+            }
+        }
+    }
 }
