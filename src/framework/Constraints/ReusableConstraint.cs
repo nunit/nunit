d46c05c884893e23b560f22b1661886aab448d2d
Charlie Poole <charlie@nunit.com>
2010-07-26 10:20:51 -0700

    Add ReusableConstraint experimental class
    --HG--
    extra : convert_revision : charlie%40nunit.com-20100726172051-2zwbrk0h18iw8ac0

---
+++ src/framework/Constraints/ReusableConstraint.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NUnit.Framework.Constraints
+{
+    public class ReusableConstraint : IResolveConstraint
+    {
+        private Constraint constraint;
+
+        public ReusableConstraint(IResolveConstraint c)
+        {
+            this.constraint = c.Resolve();
+        }
+
+        public static implicit operator ReusableConstraint(Constraint c)
+        {
+            return new ReusableConstraint(c);
+        }
+
+        public override string ToString()
+        {
+            return constraint.ToString();
+        }
+
+        #region IResolveConstraint Members
+
+        public Constraint Resolve()
+        {
+            return constraint;
+        }
+
+        #endregion
+    }
+}
---
+++ src/framework/Constraints/ReusableConstraintTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace NUnit.Framework.Constraints
+{
+    [TestFixture]
+    public class ReusableConstraintTests
+    {
+        [Datapoints]
+        static readonly ReusableConstraint[] constraints = new ReusableConstraint[] {
+            Is.Not.Empty,
+            Is.Not.Null,
+            Has.Length.GreaterThan(3),
+            Has.Property("Length").EqualTo(4).And.StartsWith("te")
+        };
+
+        [Theory]
+        public void CanReuseReusableConstraintMultipleTimes(ReusableConstraint c)
+        {
+            string s = "test";
+
+            Assume.That(s, c);
+
+            Assert.That(s, c, "Should pass first time");
+            Assert.That(s, c, "Should pass second time");
+            Assert.That(s, c, "Should pass third time");
+        }
+
+        [Test]
+        public void CanCreateReusableConstraintByImplicitConversion()
+        {
+            ReusableConstraint c = Is.Not.Null;
+
+            string s = "test";
+            Assert.That(s, c, "Should pass first time");
+            Assert.That(s, c, "Should pass second time");
+            Assert.That(s, c, "Should pass third time");
+        }
+    }
+}
