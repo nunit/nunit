3273425c67b20319c4b4c796677ccff0c49ff37f
Said Outgajjouft <saidunit@gmail.com>
2010-09-20 22:34:07 +0200

    Updated the License information for added files.
    --HG--
    branch : my working copy of nunit-3.0
    extra : convert_revision : saidunit%40gmail.com-20100920203407-9j8a8jzlgxoeq6gr

--- src/framework/Constraints/ResolvableConstraintExpression.cs
+++ src/framework/Constraints/ResolvableConstraintExpression.cs
@@ -21,8 +21,6 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // ***********************************************************************
 
-using System;
-
 namespace NUnit.Framework.Constraints
 {
     /// <summary>
