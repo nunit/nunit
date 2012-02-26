d993a48b84ed69ec9d4d0bd317834fbe94485ab7
Charlie Poole <charlie@nunit.org>
2012-02-25 23:11:44 -0800

    Put class in a namespace
    --HG--
    extra : convert_revision : charlie%40nunit.org-20120226071144-qm4msqvnggus7bjp

--- NUnitFramework/src/framework/Constraints/ToleranceMode.cs
+++ NUnitFramework/src/framework/Constraints/ToleranceMode.cs
@@ -21,31 +21,34 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // ***********************************************************************
 
-/// <summary>
-/// Modes in which the tolerance value for a comparison can be interpreted.
-/// </summary>
-public enum ToleranceMode
+namespace NUnit.Framework.Constraints
 {
     /// <summary>
-    /// The tolerance was created with a value, without specifying 
-    /// how the value would be used. This is used to prevent setting
-    /// the mode more than once and is generally changed to Linear
-    /// upon execution of the test.
+    /// Modes in which the tolerance value for a comparison can be interpreted.
     /// </summary>
-    None,
-    /// <summary>
-    /// The tolerance is used as a numeric range within which
-    /// two compared values are considered to be equal.
-    /// </summary>
-    Linear,
-    /// <summary>
-    /// Interprets the tolerance as the percentage by which
-    /// the two compared values my deviate from each other.
-    /// </summary>
-    Percent,
-    /// <summary>
-    /// Compares two values based in their distance in
-    /// representable numbers.
-    /// </summary>
-    Ulps
+    public enum ToleranceMode
+    {
+        /// <summary>
+        /// The tolerance was created with a value, without specifying 
+        /// how the value would be used. This is used to prevent setting
+        /// the mode more than once and is generally changed to Linear
+        /// upon execution of the test.
+        /// </summary>
+        None,
+        /// <summary>
+        /// The tolerance is used as a numeric range within which
+        /// two compared values are considered to be equal.
+        /// </summary>
+        Linear,
+        /// <summary>
+        /// Interprets the tolerance as the percentage by which
+        /// the two compared values my deviate from each other.
+        /// </summary>
+        Percent,
+        /// <summary>
+        /// Compares two values based in their distance in
+        /// representable numbers.
+        /// </summary>
+        Ulps
+    }
 }
