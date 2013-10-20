7f4d16c03a16b0b451bcb6c370bd27c6b4c41a3b
Henrik <henrik@haf.se>
2013-10-20 21:40:08 +0200

    first stab at writing tests for writing tests for async in F#

---
+++ src/tests/Internal/AsyncFSharpTestMethodTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal.Builders;
+using NUnit.TestUtilities;
+
+namespace NUnit.Framework.Internal
+{
+    [TestFixture]
+    public class AsyncFSharpTestMethodTests
+    {
+        NUnitTestCaseBuilder _builder;
+        object _testObject;
+
+        [SetUp]
+        public void Setup()
+        {
+            _builder = new NUnitTestCaseBuilder();
+            _testObject = typeof(FsharpAsyncTestFixture);
+        }
+
+        public IEnumerable TestCases
+        {
+            get
+            {
+                yield return new object[] { Method("async int success"), ResultState.Success, 0 };
+                yield return new object[] { Method("async unit success"), ResultState.Success, 0 };
+                yield return new object[] { Method("async tailcall success"), ResultState.Success, 0 };
+                yield return new object[] { Method("async single true assert success"), ResultState.Success, 1 };
+                yield return new object[] { Method("async multiple asserts success"), ResultState.Success, 2 };
+                yield return new object[] { Method("calling into async code success"), ResultState.Success, 0 };
+
+                yield return new object[] { Method("when throwing exception fails"), ResultState.Failure, 0 };
+                yield return new object[] { Method("async single false assert failure"), ResultState.Failure, 1 };
+            }
+        }
+
+        [Test]
+        [TestCaseSource("TestCases")]
+        public void RunTests(MethodInfo method, ResultState resultState, int assertionCount)
+        {
+            var test = _builder.BuildFrom(method);
+            var result = TestBuilder.RunTest(test, _testObject);
+
+            Assert.That(result.ResultState, Is.EqualTo(resultState), "Wrong result state");
+            Assert.That(result.AssertCount, Is.EqualTo(assertionCount), "Wrong assertion count");
+        }
+
+        static MethodInfo Method(string name)
+        {
+            return typeof(FsharpAsyncTestFixture).GetMethod(name);
+        }
+    }
+}
