74d7c9ed7f01114cbdae8168013b47b89aaab18a
Simone Busoli <simone.busoli@gmail.com>
2014-02-21 17:52:27 +0100

    Build using xbuild on Windows for mono

--- runbuild.cmd
+++ runbuild.cmd
@@ -1,4 +1,6 @@
-@@@ setlocal
-@@@ for /f %%i in ('dir /b /ad /on "%windir%\Microsoft.NET\Framework\v*"') do @if exist "%windir%\Microsoft.NET\Framework\%%i\msbuild".exe set msbuild=%windir%\Microsoft.NET\Framework\%%i\msbuild.exe
-@@@ if not defined msbuild (echo MSBuild.exe not found>&2 & exit /b 42)
-@@@ if defined msbuild "%msbuild%" NUnitFramework.msbuild %*
+@echo off
+setlocal
+for %%a in (%*) do echo "%%a" | findstr /C:"mono">nul && set buildtool=xbuild.bat
+if not defined buildtool for /f %%i in ('dir /b /ad /on "%windir%\Microsoft.NET\Framework\v*"') do @if exist "%windir%\Microsoft.NET\Framework\%%i\msbuild".exe set buildtool=%windir%\Microsoft.NET\Framework\%%i\msbuild.exe
+if not defined buildtool (echo no MSBuild.exe or xbuild was found>&2 & exit /b 42)
+if defined buildtool "%buildtool%" %~dp0NUnitFramework.msbuild %*
