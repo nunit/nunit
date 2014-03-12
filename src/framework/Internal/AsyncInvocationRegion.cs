897dd1275ae1c4143c1bdd069fa1daf53aa923ff
Simone Busoli <simone.busoli@gmail.com>
2014-03-12 14:47:05 +0100

    Removed unused Linq namespace import

--- src/framework/Internal/AsyncInvocationRegion.cs
+++ src/framework/Internal/AsyncInvocationRegion.cs
@@ -21,7 +21,6 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // ***********************************************************************
 
-using System.Linq;
 #if NET_4_0 || NET_4_5
 using System;
 using System.Collections.Generic;
