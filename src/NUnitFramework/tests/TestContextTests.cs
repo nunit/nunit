bdc90da666be75b7871952d5823cc2294a097142
Rob Prouse <rob@prouse.org>
2014-09-17 12:09:03 -0400

    Fix the failing unit test that was hidden by this issue.

--- src/NUnitFramework/tests/TestContextTests.cs
+++ src/NUnitFramework/tests/TestContextTests.cs
@@ -233,7 +233,7 @@ namespace NUnit.Framework.Tests
             Assert.That(context.Result.Outcome, Is.EqualTo(ResultState.Success));
             Assert.That(context.Result.PassCount, Is.EqualTo(2));
             Assert.That(context.Result.FailCount, Is.EqualTo(0));
-            Assert.That(context.Result.SkipCount, Is.EqualTo(1));
+            Assert.That(context.Result.SkipCount, Is.EqualTo(0));
         }
     }
 }
