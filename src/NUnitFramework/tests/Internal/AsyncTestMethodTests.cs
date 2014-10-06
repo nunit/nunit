60c349c3610b987b718f8c5529cd66a8075384b8
Charlie Poole <charliepoole@gmail.com>
2014-10-06 14:10:07 -0700

    Fix async test errors in 4.0 and 4.5 build

--- src/NUnitFramework/tests/Internal/AsyncTestMethodTests.cs
+++ src/NUnitFramework/tests/Internal/AsyncTestMethodTests.cs
@@ -27,55 +27,55 @@ namespace NUnit.Framework.Internal
         {
             get
             {
-				yield return GetTestCase(Method("AsyncVoid"), ResultState.NotRunnable, 0, false);
+                yield return GetTestCase(Method("AsyncVoid"), ResultState.NotRunnable, 0, false);
 
-				yield return GetTestCase(Method("AsyncTaskSuccess"), ResultState.Success, 1, true);
-				yield return GetTestCase(Method("AsyncTaskFailure"), ResultState.Failure, 1, true);
-				yield return GetTestCase(Method("AsyncTaskError"), ResultState.Error, 0, false);
+                yield return GetTestCase(Method("AsyncTaskSuccess"), ResultState.Success, 1, true);
+                yield return GetTestCase(Method("AsyncTaskFailure"), ResultState.Failure, 1, true);
+                yield return GetTestCase(Method("AsyncTaskError"), ResultState.Error, 0, false);
 
-				yield return GetTestCase(Method("TaskSuccess"), ResultState.Success, 1, true);
-				yield return GetTestCase(Method("TaskFailure"), ResultState.Failure, 1, true);
-				yield return GetTestCase(Method("TaskError"), ResultState.Error, 0, false);
+                yield return GetTestCase(Method("TaskSuccess"), ResultState.Success, 1, true);
+                yield return GetTestCase(Method("TaskFailure"), ResultState.Failure, 1, true);
+                yield return GetTestCase(Method("TaskError"), ResultState.Error, 0, false);
 
-				yield return GetTestCase(Method("AsyncTaskResult"), ResultState.NotRunnable, 0, false);
-				yield return GetTestCase(Method("TaskResult"), ResultState.NotRunnable, 0, false);
+                yield return GetTestCase(Method("AsyncTaskResult"), ResultState.NotRunnable, 0, false);
+                yield return GetTestCase(Method("TaskResult"), ResultState.NotRunnable, 0, false);
 
-				yield return GetTestCase(Method("AsyncTaskResultCheckSuccess"), ResultState.Success, 1, false);
-				yield return GetTestCase(Method("AsyncTaskResultCheckFailure"), ResultState.Failure, 1, false);
-				yield return GetTestCase(Method("AsyncTaskResultCheckError"), ResultState.Failure, 0, false);
+                yield return GetTestCase(Method("AsyncTaskResultCheckSuccess"), ResultState.Success, 1, false);
+                yield return GetTestCase(Method("AsyncTaskResultCheckFailure"), ResultState.ChildFailure, 1, false);
+                yield return GetTestCase(Method("AsyncTaskResultCheckError"), ResultState.ChildFailure, 0, false);
 
-				yield return GetTestCase(Method("TaskResultCheckSuccess"), ResultState.Success, 1, false);
-				yield return GetTestCase(Method("TaskResultCheckFailure"), ResultState.Failure, 1, false);
-				yield return GetTestCase(Method("TaskResultCheckError"), ResultState.Failure, 0, false);
+                yield return GetTestCase(Method("TaskResultCheckSuccess"), ResultState.Success, 1, false);
+                yield return GetTestCase(Method("TaskResultCheckFailure"), ResultState.ChildFailure, 1, false);
+                yield return GetTestCase(Method("TaskResultCheckError"), ResultState.ChildFailure, 0, false);
 
-				yield return GetTestCase(Method("AsyncTaskTestCaseWithParametersSuccess"), ResultState.Success, 1, true);
-				yield return GetTestCase(Method("AsyncTaskResultCheckSuccessReturningNull"), ResultState.Success, 1, false);
-				yield return GetTestCase(Method("TaskResultCheckSuccessReturningNull"), ResultState.Success, 1, false);
+                yield return GetTestCase(Method("AsyncTaskTestCaseWithParametersSuccess"), ResultState.Success, 1, true);
+                yield return GetTestCase(Method("AsyncTaskResultCheckSuccessReturningNull"), ResultState.Success, 1, false);
+                yield return GetTestCase(Method("TaskResultCheckSuccessReturningNull"), ResultState.Success, 1, false);
                 
-				yield return GetTestCase(Method("NestedAsyncTaskSuccess"), ResultState.Success, 1, false);
-				yield return GetTestCase(Method("NestedAsyncTaskFailure"), ResultState.Failure, 1, true);
-				yield return GetTestCase(Method("NestedAsyncTaskError"), ResultState.Error, 0, false);
+                yield return GetTestCase(Method("NestedAsyncTaskSuccess"), ResultState.Success, 1, false);
+                yield return GetTestCase(Method("NestedAsyncTaskFailure"), ResultState.Failure, 1, true);
+                yield return GetTestCase(Method("NestedAsyncTaskError"), ResultState.Error, 0, false);
 
-				yield return GetTestCase(Method("AsyncTaskMultipleSuccess"), ResultState.Success, 1, true);
-				yield return GetTestCase(Method("AsyncTaskMultipleFailure"), ResultState.Failure, 1, true);
-				yield return GetTestCase(Method("AsyncTaskMultipleError"), ResultState.Error, 0, false);
+                yield return GetTestCase(Method("AsyncTaskMultipleSuccess"), ResultState.Success, 1, true);
+                yield return GetTestCase(Method("AsyncTaskMultipleFailure"), ResultState.Failure, 1, true);
+                yield return GetTestCase(Method("AsyncTaskMultipleError"), ResultState.Error, 0, false);
 
-				yield return GetTestCase(Method("TaskCheckTestContextAcrossTasks"), ResultState.Success, 2, true);
-				yield return GetTestCase(Method("TaskCheckTestContextWithinTestBody"), ResultState.Success, 2, true);
+                yield return GetTestCase(Method("TaskCheckTestContextAcrossTasks"), ResultState.Success, 2, true);
+                yield return GetTestCase(Method("TaskCheckTestContextWithinTestBody"), ResultState.Success, 2, true);
             }
         }
 
-		/// <summary>
-		/// Private method to return a test case, optionally ignored on the Linux platform.
-		/// We use this since the Platform attribute is not supported on TestCaseData.
-		/// </summary>
-		private TestCaseData GetTestCase(MethodInfo method, ResultState resultState, int assertionCount, bool ignoreOnLinux)
-		{
-			var data = new TestCaseData(method, resultState, assertionCount);
-			if (ON_LINUX && ignoreOnLinux)
-				data = data.Ignore("Intermittent failure on Linux");
-			return data;
-		}
+        /// <summary>
+        /// Private method to return a test case, optionally ignored on the Linux platform.
+        /// We use this since the Platform attribute is not supported on TestCaseData.
+        /// </summary>
+        private TestCaseData GetTestCase(MethodInfo method, ResultState resultState, int assertionCount, bool ignoreOnLinux)
+        {
+            var data = new TestCaseData(method, resultState, assertionCount);
+            if (ON_LINUX && ignoreOnLinux)
+                data = data.Ignore("Intermittent failure on Linux");
+            return data;
+        }
 
         [Test]
         [TestCaseSource("TestCases")]
