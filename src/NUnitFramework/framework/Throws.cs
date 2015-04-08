d0ddb59852f9c0e71d067809c045ea833ad6c1f8
Henning Moe <hem@isys.no>
2015-04-08 09:51:52 +0200

    Fix XML documentation on Throws.ArgumentException, Throws.InvalidOperationException. Fixes #577.

--- src/NUnitFramework/framework/Throws.cs
+++ src/NUnitFramework/framework/Throws.cs
@@ -72,7 +72,7 @@ namespace NUnit.Framework
         #region ArgumentException
 
         /// <summary>
-        /// Creates a constraint specifying an expected TargetInvocationException
+        /// Creates a constraint specifying an expected ArgumentException
         /// </summary>
         public static ExactTypeConstraint ArgumentException
         {
@@ -96,7 +96,7 @@ namespace NUnit.Framework
         #region InvalidOperationException
 
         /// <summary>
-        /// Creates a constraint specifying an expected TargetInvocationException
+        /// Creates a constraint specifying an expected InvalidOperationException
         /// </summary>
         public static ExactTypeConstraint InvalidOperationException
         {
