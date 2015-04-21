f3a98980768e47948820f23fd8d59abfe60a98f1
Rob Prouse <rob@prouse.org>
2015-04-21 15:39:23 -0400

    Proposed solution for Issue #600

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -22,6 +22,7 @@
 // ***********************************************************************
 
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 //
 // Common Information for the NUnit assemblies
@@ -30,6 +31,14 @@ using System.Reflection;
 [assembly: AssemblyProduct("NUnit 3.0")]
 [assembly: AssemblyCopyright("Copyright (C) 2014 Charlie Poole")]
 [assembly: AssemblyTrademark("NUnit is a trademark of NUnit Software")]
+
+[assembly: InternalsVisibleTo("nunit.framework.tests, PublicKey=002400000480000094" +
+                              "000000060200000024000052534131000400000100010031eea" +
+                              "370b1984bfa6d1ea760e1ca6065cee41a1a279ca234933fe977" +
+                              "a096222c0e14f9e5a17d5689305c6d7f1206a85a53c48ca0100" +
+                              "80799d6eeef61c98abd18767827dc05daea6b6fbd2e868410d9" +
+                              "bee5e972a004ddd692dec8fa404ba4591e847a8cf35de21c2d3" +
+                              "723bc8d775a66b594adeb967537729fe2a446b548cd57a6")]
 
 #if DEBUG
 #if NET_4_5
