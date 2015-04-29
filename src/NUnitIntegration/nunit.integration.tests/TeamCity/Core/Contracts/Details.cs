d6dca3678206c306bba1814fc3cdd6a7b5a5ddda
Nikolay Pianikov <NikolayP@live.com>
2015-04-29 12:26:03 +0300

    610: fix code style

--- src/NUnitIntegration/nunit.integration.tests/TeamCity/Core/Contracts/Details.cs
+++ src/NUnitIntegration/nunit.integration.tests/TeamCity/Core/Contracts/Details.cs
@@ -1,4 +1,27 @@
-﻿using System;
+﻿// ***********************************************************************
+// Copyright (c) 2014 Charlie Poole
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// ***********************************************************************
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +30,7 @@ using JetBrains.Annotations;
 
 namespace NUnit.Integration.Tests.TeamCity.Core.Contracts
 {
-    public class Details
+    public sealed class Details
     {
         private readonly List<string> _details = new List<string>();
 
@@ -25,16 +48,20 @@ namespace NUnit.Integration.Tests.TeamCity.Core.Contracts
             _details.AddRange(detailLines);
         }
 
+        [NotNull]
         public Details Combine([NotNull] Details otherDetails, string prefix = "")
         {
             Contract.Requires<ArgumentNullException>(otherDetails != null);
+            Contract.Ensures(Contract.Result<Details>() != null);
 
             return new Details(_details.Concat(otherDetails._details.Select(i => prefix + i)));
         }
 
+        [NotNull]
         public Details Combine([NotNull] IEnumerable<string> detailLines, string prefix = "")
         {
             Contract.Requires<ArgumentNullException>(detailLines != null);
+            Contract.Ensures(Contract.Result<Details>() != null);
 
             return new Details(_details.Concat(detailLines.Select(i => prefix + i)));
         }
