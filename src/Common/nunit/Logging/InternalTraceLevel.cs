c0d3658eedabb204559323a0632157a35ca9ecd7
Charlie Poole <charliepoole@gmail.com>
2015-05-06 22:39:36 -0700

    Add interfaces from nunit.engine.api to the core engine directly

--- src/Common/nunit/Logging/InternalTraceLevel.cs
+++ src/Common/nunit/Logging/InternalTraceLevel.cs
@@ -21,7 +21,7 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // ***********************************************************************
 
-#if NUNIT_ENGINE
+#if NUNIT_ENGINE || CORE_ENGINE
 namespace NUnit.Engine
 #elif NUNIT_FRAMEWORK
 namespace NUnit.Framework.Internal
