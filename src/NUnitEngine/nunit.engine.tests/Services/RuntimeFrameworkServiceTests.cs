729700c7da28de7e2be6524c7f5b81a69147200b
Charlie Poole <charliepoole@gmail.com>
2015-08-06 06:58:59 -0700

    Modify tests to check CLR version

--- src/NUnitEngine/nunit.engine.tests/Services/RuntimeFrameworkServiceTests.cs
+++ src/NUnitEngine/nunit.engine.tests/Services/RuntimeFrameworkServiceTests.cs
@@ -53,27 +53,32 @@ namespace NUnit.Engine.Services.Tests
             Assert.That(_runtimeService.Status, Is.EqualTo(ServiceStatus.Started));
         }
 
-        [TestCase("mock-nunit-assembly.exe", "net-2.0", false)]
-        [TestCase("net-2.0/mock-nunit-assembly.exe", "net-2.0", false)]
-        [TestCase("net-4.0/mock-nunit-assembly.exe", "net-4.0", false)]
+        [TestCase("mock-nunit-assembly.exe", "2.0.50727", false)]
+        [TestCase("net-2.0/mock-nunit-assembly.exe", "2.0.50727", false)]
+        [TestCase("net-4.0/mock-nunit-assembly.exe", "4.0.30319", false)]
         // TODO: Change this case when the 4.0/4.5 bug is fixed
-        [TestCase("net-4.5/mock-nunit-assembly.exe", "net-4.0", false)]
-        //[TestCase("mock-cpp-clr-x64.dll", "net-4.0", false)]
-        //[TestCase("mock-cpp-clr-x86.dll", "net-4.0", true)]
-        [TestCase("nunit-agent.exe", "net-2.0", false)]
-        [TestCase("nunit-agent-x86.exe", "net-2.0", true)]
+        [TestCase("net-4.5/mock-nunit-assembly.exe", "4.0.30319", false)]
+        [TestCase("mock-cpp-clr-x64.dll", "4.0.30319", false)]
+        [TestCase("mock-cpp-clr-x86.dll", "4.0.30319", true)]
+        [TestCase("nunit-agent.exe", "2.0.50727", false)]
+        [TestCase("nunit-agent-x86.exe", "2.0.50727", true)]
         // TODO: Make the following cases work correctly in case we want to use
         // the engine to run them in the future.
-        //[TestCase("netcf-3.5/mock-nunit-assembly.exe", "net-2.0", false)]
-        //[TestCase("sl-5.0/mock-nunit-assembly.dll", "net-4.0", false)]
-        //[TestCase("portable/mock-nunit-assembly.dll", "net-4.0", false)]
-        public void SelectRuntimeFramework(string assemblyName, string expectedRuntime, bool runAsX86)
+        [TestCase("netcf-3.5/mock-nunit-assembly.exe", "2.0.50727", false)]
+        [TestCase("sl-5.0/mock-nunit-assembly.dll", "4.0.30319", false)]
+        [TestCase("portable/mock-nunit-assembly.dll", "4.0.30319", false)]
+        public void SelectRuntimeFramework(string assemblyName, string expectedVersion, bool runAsX86)
         {
+            // Some files don't actually exist on our CI servers
+            Assume.That(assemblyName, Does.Exist);
             var package = new TestPackage(assemblyName);
-            if (RuntimeFramework.CurrentFramework.Runtime == RuntimeType.Mono)
-                expectedRuntime = expectedRuntime.Replace("net", "mono");
-            Assert.That(_runtimeService.SelectRuntimeFramework(package), Is.EqualTo(expectedRuntime));
+
+            var returnValue = _runtimeService.SelectRuntimeFramework(package);
+            var framework = RuntimeFramework.Parse(returnValue);
+
+            Assert.That(package.GetSetting("RuntimeFramework", ""), Is.EqualTo(returnValue));
             Assert.That(package.GetSetting("RunAsX86", false), Is.EqualTo(runAsX86));
+            Assert.That(framework.ClrVersion.ToString(), Is.EqualTo(expectedVersion));
         }
     }
 }
