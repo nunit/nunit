f52d1fa9021b88fd242defcc2ed3c00b9abb84e1
Rob Prouse <rob@prouse.org>
2015-10-02 10:45:40 -0400

    Added the .NET core test data project

--- src/NUnitFramework/testdata/TestFixtureData.cs
+++ src/NUnitFramework/testdata/TestFixtureData.cs
@@ -484,7 +484,7 @@ namespace NUnit.TestData.TestFixtureTests
         }
     }
 
-#if !NETCF && !SILVERLIGHT && !PORTABLE
+#if !NETCF && !SILVERLIGHT && !PORTABLE && !NETCORE
     [TestFixture]
     public class FixtureThatChangesTheCurrentPrincipal
     {
