0836e573feabcb57adab009dc1f20258efc04ce7
Rob Prouse <rob@prouse.org>
2015-10-13 18:39:07 -0400

    Non-working DNX runner for the .NET Core tests

---
+++ src/NUnitFramework/nunit.dnx.tests/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NUnitLite.Runner
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            return new AutoRun().Execute(typeof(Program).Assembly, Console.Out, Console.In, args);
+        }
+    }
+}
