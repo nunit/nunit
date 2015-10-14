e4c83493a24aa62cd7c1693ae3313edd699b817e
Rob Prouse <rob@prouse.org>
2015-10-14 08:12:22 -0400

    Removed improper references from the DNX framework project.json

--- src/NUnitFramework/framework/Is.cs
+++ src/NUnitFramework/framework/Is.cs
@@ -173,7 +173,7 @@ namespace NUnit.Framework
 
         #region XmlSerializable
 
-#if !SILVERLIGHT
+#if !SILVERLIGHT && !NETCORE
         /// <summary>
         /// Returns a constraint that tests whether an object graph is serializable in xml format.
         /// </summary>
