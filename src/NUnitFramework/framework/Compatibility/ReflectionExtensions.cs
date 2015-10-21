7fc930e70f2d5ff82de55e94583974ac75d0221b
Rob Prouse <rob@prouse.org>
2015-10-21 08:52:23 -0400

    Fixed the ParametersMatch extension method to work with implicitly castable value types

--- src/NUnitFramework/framework/Compatibility/ReflectionExtensions.cs
+++ src/NUnitFramework/framework/Compatibility/ReflectionExtensions.cs
@@ -320,10 +320,40 @@ namespace NUnit.Framework.Compatibility
 
             for (int i = 0; i < pinfos.Length; i++)
             {
-                if (!pinfos[i].ParameterType.Equals(ptypes[i]))
+                if (!pinfos[i].ParameterType.IsCastableFrom(ptypes[i]))
                     return false;
             }
             return true;
+        }
+
+        static Dictionary<Type, List<Type>> convertibleValueTypes = new Dictionary<Type, List<Type>>() {
+            { typeof(decimal), new List<Type> { typeof(sbyte), typeof(byte), typeof(short), typeof(ushort), typeof(int), typeof(uint), typeof(long), typeof(ulong), typeof(char) } },
+            { typeof(double), new List<Type> { typeof(sbyte), typeof(byte), typeof(short), typeof(ushort), typeof(int), typeof(uint), typeof(long), typeof(ulong), typeof(char), typeof(float) } },
+            { typeof(float), new List<Type> { typeof(sbyte), typeof(byte), typeof(short), typeof(ushort), typeof(int), typeof(uint), typeof(long), typeof(ulong), typeof(char), typeof(float) } },
+            { typeof(ulong), new List<Type> { typeof(byte), typeof(ushort), typeof(uint), typeof(char) } },
+            { typeof(long), new List<Type> { typeof(sbyte), typeof(byte), typeof(short), typeof(ushort), typeof(int), typeof(uint), typeof(char) } },
+            { typeof(uint), new List<Type> { typeof(byte), typeof(ushort), typeof(char) } },
+            { typeof(int), new List<Type> { typeof(sbyte), typeof(byte), typeof(short), typeof(ushort), typeof(char) } },
+            { typeof(ushort), new List<Type> { typeof(byte), typeof(char) } },
+            { typeof(short), new List<Type> { typeof(byte) } }
+        };
+
+        /// <summary>
+        /// Determines if one type can be implicitly converted from another
+        /// </summary>
+        /// <param name="to"></param>
+        /// <param name="from"></param>
+        /// <returns></returns>
+        public static bool IsCastableFrom(this Type to, Type from)
+        {
+            if (to.IsAssignableFrom(from))
+                return true;
+
+            if (convertibleValueTypes.ContainsKey(to) && convertibleValueTypes[to].Contains(from))
+                return true;
+
+            return from.GetMethods(BindingFlags.Public | BindingFlags.Static)
+                       .Any(m => m.ReturnType == to && m.Name == "op_Implicit");
         }
     }
 
@@ -391,7 +421,7 @@ namespace NUnit.Framework.Compatibility
     /// Extensions for Assembly that are not available in portable
     /// </summary>
     public static class AssemblyExtensions
-    { 
+    {
         /// <summary>
         /// DNX does not have a version of GetCustomAttributes on Assembly that takes an inherit
         /// parameter since it doesn't make sense on Assemblies. This version just ignores the 
