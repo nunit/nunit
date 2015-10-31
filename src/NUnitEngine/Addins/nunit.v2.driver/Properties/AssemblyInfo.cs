ed4f186e64d2bee424ee2821404beab5d9892245
Rob Prouse <rob@prouse.org>
2015-10-31 14:06:03 -0400

    Update copyright to 2015 for the release. Fixes #949

--- src/NUnitEngine/Addins/nunit.v2.driver/Properties/AssemblyInfo.cs
+++ src/NUnitEngine/Addins/nunit.v2.driver/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("nunit.v2.driver")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
