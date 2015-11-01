54d35a9f56b34836d137bc7aa1ee1cc038a11a76
Charlie Poole <charliepoole@gmail.com>
2015-11-01 08:47:41 -0800

    Update copyright lines to 2015

--- src/NUnitEngine/Addins/nunit.v2.driver.tests/Properties/AssemblyInfo.cs
+++ src/NUnitEngine/Addins/nunit.v2.driver.tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("mock-v2-assembly")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
