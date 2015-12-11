e07a5b112d2e315ef11e8897a20b4387d1c24363
Rob Prouse <rob@prouse.org>
2015-12-10 22:05:35 -0500

    Added a console writeline to output on travis

--- src/NUnitEngine/nunit.engine/Services/TestAgency.cs
+++ src/NUnitEngine/nunit.engine/Services/TestAgency.cs
@@ -202,6 +202,7 @@ namespace NUnit.Engine.Services
                 case RuntimeType.Mono:
                     p.StartInfo.FileName = NUnitConfiguration.MonoExePath;
                     string monoOptions = "--runtime=v" + targetRuntime.ClrVersion.ToString();
+                    Console.WriteLine("Mono Options: " + monoOptions);
                     if (debugTests || debugAgent) monoOptions += " --debug";
                     p.StartInfo.Arguments = string.Format("{0} \"{1}\" {2}", monoOptions, agentExePath, arglist);
                     break;
