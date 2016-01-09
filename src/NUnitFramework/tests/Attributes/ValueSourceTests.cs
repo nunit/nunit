5851da976e8ac227b83b140ffd1ced8a5d2fce96
Victor Gladkikh <me@alfeg.net>
2016-01-09 20:11:41 +0300

    #1121 : ValueSourceAttribute.GetDataSource should throw instead of returning null

--- src/NUnitFramework/tests/Attributes/ValueSourceTests.cs
+++ src/NUnitFramework/tests/Attributes/ValueSourceTests.cs
@@ -24,7 +24,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using NUnit.Framework.Compatibility;
 using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+using NUnit.Framework.Internal.Builders;
 using NUnit.TestUtilities;
 
 namespace NUnit.Framework.Attributes
@@ -167,6 +171,51 @@ namespace NUnit.Framework.Attributes
                 dataList.Add(8);
 
                 return dataList;
+            }
+
+            public static IEnumerable<int> ForeignNullResultProvider()
+            {
+                return null;
+            }
+        }
+
+        public static string NullSource = null;
+
+        public static IEnumerable<int> NullDataSourceProvider()
+        {
+            return null;
+        }
+
+        public static IEnumerable<int> NullDataSourceProperty
+        {
+            get { return null; }
+        }
+
+        [Test, Explicit("Null or nonexisting data sources definitions should not prevent other tests from run #1121")]
+        public void ValueSourceMayNotBeNull(
+            [ValueSource("NullSource")] string nullSource,
+            [ValueSource("NullDataSourceProvider")] string nullDataSourceProvided,
+            [ValueSource(typeof(ValueProvider), "ForeignNullResultProvider")] string nullDataSourceProvider,
+            [ValueSource("NullDataSourceProperty")] int nullDataSourceProperty,
+            [ValueSource("SomeNonExistingMemberSource")] int nonExistingMember)
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void ValueSourceAttributeShouldThrowInsteadOfReturningNull()
+        {
+            var method = new MethodWrapper(GetType(), "ValueSourceMayNotBeNull");
+            var parameters = method.GetParameters();
+
+            foreach (var parameter in parameters)
+            {
+                var dataSource = parameter.GetCustomAttributes<IParameterDataSource>(false)[0];
+
+                Assert.Throws<InvalidDataSourceException>(() =>
+                {
+                    var data = dataSource.GetData(parameter);
+                }); 
             }
         }
     }
