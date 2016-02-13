a2d06afc881305a6121ad35ce9528d4143bd1f4d
Joel Dickson <j.dickson@hosted-solutions.com.au>
2016-02-13 16:13:58 +0700

    remove unused reference

--- src/NUnitFramework/framework/Attributes/TestAttribute.cs
+++ src/NUnitFramework/framework/Attributes/TestAttribute.cs
@@ -21,7 +21,6 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // ***********************************************************************
 
-using System.Collections.Concurrent;
 
 namespace NUnit.Framework
 {
