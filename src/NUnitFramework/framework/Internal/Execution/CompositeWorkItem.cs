71fdd2c1a429e7aad812bd9fa12bdcabee2c9ddd
Joel Dickson <j.dickson@hosted-solutions.com.au>
2016-02-22 07:53:23 +0700

    Change to sort first N items

--- src/NUnitFramework/framework/Internal/Execution/CompositeWorkItem.cs
+++ src/NUnitFramework/framework/Internal/Execution/CompositeWorkItem.cs
@@ -45,11 +45,15 @@ namespace NUnit.Framework.Internal.Execution
         private TestCommand _setupCommand;
         private TestCommand _teardownCommand;
 
+        /// <summary>
+        /// List of Child WorkItems
+        /// </summary>
         public List<WorkItem> Children;
+
         /// <summary>
         /// A count of how many tests in the work item have a value for the Order Property
         /// </summary>
-        public int CountOrder;
+        private int _countOrder;
 
         private CountdownEvent _childTestCountdown;
 
@@ -64,7 +68,7 @@ namespace NUnit.Framework.Internal.Execution
         {
             _suite = suite;
             _childFilter = childFilter;
-            CountOrder = 0;
+            _countOrder = 0;
         }
 
         /// <summary>
@@ -239,13 +243,38 @@ namespace NUnit.Framework.Internal.Execution
             Children = new List<WorkItem>();
 
             foreach (ITest test in _suite.Tests)
-                if (_childFilter.Pass(test))
+                if (_childFilter.Pass(test) && test.Properties.ContainsKey(PropertyNames.Order))
                 {
-                    Children.Add(WorkItem.CreateWorkItem(test, _childFilter));
-                    if (test.Properties.ContainsKey(PropertyNames.Order)) CountOrder++;
+                        Children.Add(WorkItem.CreateWorkItem(test, _childFilter));
+                        _countOrder++;
                 }
 
-            if (CountOrder !=0) SortChildren();
+            foreach (ITest test in _suite.Tests)
+                if (_childFilter.Pass(test) && !test.Properties.ContainsKey(PropertyNames.Order))
+                {
+                    Children.Add(WorkItem.CreateWorkItem(test, _childFilter));
+                }
+
+            if (_countOrder !=0) SortChildren();
+        }
+
+        private class WorkItemOrderComparer : IComparer<WorkItem>
+        {
+            /// <summary>
+            /// Compares two objects and returns a value indicating whether one is less than, equal to, or greater than the other.
+            /// </summary>
+            /// <returns>
+            /// A signed integer that indicates the relative values of <paramref name="x"/> and <paramref name="y"/>, as shown in the following table.Value Meaning Less than zero<paramref name="x"/> is less than <paramref name="y"/>.Zero<paramref name="x"/> equals <paramref name="y"/>.Greater than zero<paramref name="x"/> is greater than <paramref name="y"/>.
+            /// </returns>
+            /// <param name="x">The first object to compare.</param><param name="y">The second object to compare.</param>
+            public int Compare(WorkItem x, WorkItem y)
+            {
+                var xKey = int.MaxValue;
+                var yKey = int.MaxValue;
+                if (x.Test.Properties.ContainsKey(PropertyNames.Order)) int.TryParse(x.Test.Properties[PropertyNames.Order][0].ToString(), out xKey);
+                if (y.Test.Properties.ContainsKey(PropertyNames.Order)) int.TryParse(y.Test.Properties[PropertyNames.Order][0].ToString(), out yKey);
+                return xKey.CompareTo(yKey);
+            }
         }
 
         /// <summary>
@@ -253,14 +282,7 @@ namespace NUnit.Framework.Internal.Execution
         /// </summary>
         private void SortChildren()
         {
-            Children.Sort(delegate (WorkItem x, WorkItem y)
-            {
-                var xKey = int.MaxValue;
-                var yKey = int.MaxValue;
-                if (x.Test.Properties.ContainsKey(PropertyNames.Order)) int.TryParse(x.Test.Properties[PropertyNames.Order][0].ToString(), out xKey);
-                if (y.Test.Properties.ContainsKey(PropertyNames.Order)) int.TryParse(y.Test.Properties[PropertyNames.Order][0].ToString(), out yKey);
-                return xKey.CompareTo(yKey);
-            });
+            Children.Sort(0, _countOrder, new WorkItemOrderComparer());
         }
         private void SkipFixture(ResultState resultState, string message, string stackTrace)
         {
