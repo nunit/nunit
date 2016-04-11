82f2c53f77e41d455714859f272846faa770ac58
Fabrizio Steiner <fas@bbtsoftware.ch>
2016-04-11 08:42:19 +0200

    Added generic CombineTest tests for the AndFilter/OrFilter. This test checks first that the results are correctly combined and second that the expected match function is called on the sub-filters.

---
+++ src/NUnitFramework/tests/Internal/Filters/MockTestFilter.cs
@@ -0,0 +1,114 @@
+﻿// ***********************************************************************
+// Copyright (c) 2015 Charlie Poole
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// ***********************************************************************
+
+using System;
+using NUnit.Framework.Interfaces;
+
+namespace NUnit.Framework.Internal.Filters
+{
+    /// <summary>
+    /// Mocks a <see cref="TestFilter"/>. Checks that only one specific match-function 
+    /// (<see cref=MatchFunction"/> is called and only for a specific <see cref="ITest"/>.
+    /// 
+    /// For the specific test and match-function one could set the return value of the function.
+    /// Furthermore one could read out the number of valid calls to the match-function through
+    /// <see cref="NumberOfMatchCalls"/>.
+    /// 
+    /// It would be better to use a Mocking-Framework for this.
+    /// </summary>
+    [Serializable]
+    public class MockTestFilter : TestFilter
+    {
+        public enum MatchFunction
+        {
+            Match,
+            IsExplicitMatch,
+            Pass
+        }
+
+        /// <summary>
+        /// The test, for which the match-function is allowed to be called.
+        /// </summary>
+        private readonly ITest _expectedTest;
+
+        /// <summary>
+        /// The one and only match-function, which is allowed to be called.
+        /// </summary>
+        private readonly MatchFunction _expectedFunctionToBeCalled;
+
+        /// <summary>
+        /// The result of the match-function <see cref="_expectedFunctionToBeCalled"/>
+        /// for the teste <see cref="_expectedTest"/>.
+        /// </summary>
+        private readonly bool _matchFunctionResult;
+
+        /// <summary>
+        /// Gets the number of valid calls of the match-function that have been executed.
+        /// </summary>
+        public int NumberOfMatchCalls { get; private set; }
+
+        /// <summary>
+        /// Construct an <see cref="MockTestFilter"/>.
+        /// </summary>
+        /// <param name="expectedTest">The test for which calling the match-function is allowed.</param>
+        /// <param name="expectedMatchFunction">The matching function that should be called.</param>
+        /// <param name="matchFunctionResult">The result of the match function for the expected
+        /// test.</param>
+        public MockTestFilter(
+            ITest expectedTest, MatchFunction expectedMatchFunction, bool matchFunctionResult) : base()
+        {
+            _expectedTest = expectedTest;
+            _expectedFunctionToBeCalled = expectedMatchFunction;
+            _matchFunctionResult = matchFunctionResult;
+            NumberOfMatchCalls = 0;
+        }
+
+        public override bool Match(ITest test)
+        {
+            return AssertAndGetEquality(test, MatchFunction.Match);
+        }
+
+        public override bool IsExplicitMatch(ITest test)
+        {
+            return AssertAndGetEquality(test, MatchFunction.IsExplicitMatch);
+        }
+
+        public override bool Pass(ITest test)
+        {
+            return AssertAndGetEquality(test, MatchFunction.Pass);
+        }
+
+        private bool AssertAndGetEquality(ITest test, MatchFunction calledFunction)
+        {
+            Assert.AreSame(_expectedTest, test);
+            Assert.AreEqual(_expectedFunctionToBeCalled, calledFunction);
+            NumberOfMatchCalls += 1;
+            return _matchFunctionResult;
+        }
+
+        public override TNode AddToXml(TNode parentNode, bool recursive)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/NUnitFramework/tests/Internal/Filters/OrFilterTests.cs
+++ src/NUnitFramework/tests/Internal/Filters/OrFilterTests.cs
@@ -76,6 +76,75 @@ namespace NUnit.Framework.Internal.Filters
             Assert.False(filter.IsExplicitMatch(_yetAnotherFixture));
         }
 
+        /// <summary>
+        /// Generic combination tests for the <see cref="OrFilter"/>. This test checks that the
+        /// <see cref="OrFilter"/> correctly combines the results from its sub-filters.
+        /// Furthermore it checks that the result from the correct match-function of the
+        /// sub-filters is used to calculate the OR combination.
+        /// 
+        /// The input is an array of booleans (<paramref name="inputBooleans"/>). For each boolean
+        /// value a <see cref="MockTestFilter"/> is added to the <see cref="OrFilter"/>
+        /// whose match-function (defined through the paramter <paramref name="matchFunction"/>)
+        /// evaluates to the boolean value indicated.
+        /// Afterwrards the requested match-function (<paramref name="matchFunction"/>) is called
+        /// on the <see cref="OrFilter"/> and is compared to the expected result
+        /// (<paramref name="expectedResult"/>).
+        /// This tests also throws an exception if the match-function call from the
+        /// <see cref="OrFilter"/> calls not the same match-function on the
+        /// <see cref="MockTestFilter"/>, thus checking that the <see cref="OrFilter"/>
+        /// combines the correct results from the sub-filters.
+        /// 
+        /// See also <see cref="MockTestFilter"/>.
+        /// </summary>
+        [TestCase(new bool[] { false, false }, false, MockTestFilter.MatchFunction.IsExplicitMatch)]
+        [TestCase(new bool[] { true, false }, true, MockTestFilter.MatchFunction.IsExplicitMatch)]
+        [TestCase(new bool[] { false, true }, true, MockTestFilter.MatchFunction.IsExplicitMatch)]
+        [TestCase(new bool[] { true, true }, true, MockTestFilter.MatchFunction.IsExplicitMatch)]
+        [TestCase(new bool[] { false, false }, false, MockTestFilter.MatchFunction.Match)]
+        [TestCase(new bool[] { true, false }, true, MockTestFilter.MatchFunction.Match)]
+        [TestCase(new bool[] { false, true }, true, MockTestFilter.MatchFunction.Match)]
+        [TestCase(new bool[] { true, true }, true, MockTestFilter.MatchFunction.Match)]
+        [TestCase(new bool[] { false, false }, false, MockTestFilter.MatchFunction.Pass)]
+        [TestCase(new bool[] { true, false }, true, MockTestFilter.MatchFunction.Pass)]
+        [TestCase(new bool[] { false, true }, true, MockTestFilter.MatchFunction.Pass)]
+        [TestCase(new bool[] { true, true }, true, MockTestFilter.MatchFunction.Pass)]
+        public void CombineTest(IEnumerable<bool> inputBooleans, bool expectedResult,
+            MockTestFilter.MatchFunction matchFunction)
+        {
+            var filters = new List<MockTestFilter>();
+            foreach (var inputBool in inputBooleans)
+            {
+                var strictFilter = new MockTestFilter(_dummyFixture, matchFunction, inputBool);
+                Assert.AreEqual(inputBool, ExecuteMatchFunction(strictFilter, matchFunction));
+
+                filters.Add(strictFilter);
+            }
+
+            var filter = new OrFilter(filters.ToArray());
+            bool calculatedResult = ExecuteMatchFunction(filter, matchFunction);
+            Assert.AreEqual(expectedResult, calculatedResult);
+        }
+
+        /// <summary>
+        /// Executes the requested <paramref name="matchFunction"/> on the <paramref name="filter"/>.
+        /// </summary>
+        private bool ExecuteMatchFunction(
+            TestFilter filter, MockTestFilter.MatchFunction matchFunction)
+        {
+            switch (matchFunction)
+            {
+                case MockTestFilter.MatchFunction.IsExplicitMatch:
+                    return filter.IsExplicitMatch(_dummyFixture);
+                case MockTestFilter.MatchFunction.Match:
+                    return filter.Match(_dummyFixture);
+                case MockTestFilter.MatchFunction.Pass:
+                    return filter.Pass(_dummyFixture);
+                default:
+                    throw new ArgumentException(
+                        "Unexpected StrictIdFilterForTests.EqualValueFunction.", "matchFunction");
+            }
+        }
+
         [Test]
         public void BuildFromXml()
         {
