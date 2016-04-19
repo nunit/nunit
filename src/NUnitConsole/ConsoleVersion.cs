96a7ae1d76772b263193d7c9879cdc82d005445f
Rob Prouse <rob@prouse.org>
2016-04-18 20:47:56 -0400

    Updated copyright year

--- src/NUnitConsole/ConsoleVersion.cs
+++ src/NUnitConsole/ConsoleVersion.cs
@@ -26,5 +26,5 @@ using System.Reflection;
 //
 // Current version for the NUnit Console
 //
-[assembly: AssemblyVersion("3.3.0.0")]
-[assembly: AssemblyFileVersion("3.3.0.0")]
+[assembly: AssemblyVersion("3.2.1.0")]
+[assembly: AssemblyFileVersion("3.2.1.0")]
