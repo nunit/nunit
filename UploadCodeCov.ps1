fffa9b523d2ba77f2f434e4f987e946c3b242b68
Joel Dickson <j.dickson@hosted-solutions.com.au>
2017-02-12 16:22:31 +0700

    fix ps error on AppVe

--- UploadCodeCov.ps1
+++ UploadCodeCov.ps1
@@ -1,2 +1,5 @@
 (New-Object System.Net.WebClient).DownloadFile("https://codecov.io/bash", ".\CodecovUploader.sh")
-get-item opencover*.xml | ForEach-Object {.\CodecovUploader.sh -f $_.Name -t "72811e91-101d-439b-aed1-52f6297b0976" -X gcov}
+.\CodecovUploader.sh -f "opencover4.5.xml" -t "72811e91-101d-439b-aed1-52f6297b0976" -X gcov
+.\CodecovUploader.sh -f "opencover3.5.xml" -t "72811e91-101d-439b-aed1-52f6297b0976" -X gcov
+.\CodecovUploader.sh -f "opencover3.0.xml" -t "72811e91-101d-439b-aed1-52f6297b0976" -X gcov
+.\CodecovUploader.sh -f "opencover2.0.xml" -t "72811e91-101d-439b-aed1-52f6297b0976" -X gcov
