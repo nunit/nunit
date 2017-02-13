53b35007d878b9541d63af5aca7a485ec4bf382d
Joel Dickson <j.dickson@hosted-solutions.com.au>
2017-02-13 16:56:34 +0700

    remove bash upload

--- UploadCodeCov.ps1
+++
@@ -1,5 +0,0 @@
-(New-Object System.Net.WebClient).DownloadFile("https://codecov.io/bash", ".\CodecovUploader.sh")
-.\CodecovUploader.sh -f "opencover4.5.xml" -t "72811e91-101d-439b-aed1-52f6297b0976" -X gcov
-.\CodecovUploader.sh -f "opencover3.5.xml" -t "72811e91-101d-439b-aed1-52f6297b0976" -X gcov
-.\CodecovUploader.sh -f "opencover3.0.xml" -t "72811e91-101d-439b-aed1-52f6297b0976" -X gcov
-.\CodecovUploader.sh -f "opencover2.0.xml" -t "72811e91-101d-439b-aed1-52f6297b0976" -X gcov
