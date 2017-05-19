219fb928cd62f3485b45d49926f7b468c8c08dc0
Chris Maddock <chrismaddock@live.co.uk>
2017-05-19 10:17:34 +0100

    Remove Pre/PostTestAttribute, and SpecialValue remnants

--- src/NUnitFramework/framework/Attributes/SetUpAttribute.cs
+++ src/NUnitFramework/framework/Attributes/SetUpAttribute.cs
@@ -32,22 +32,4 @@ namespace NUnit.Framework
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited=true)]
     public class SetUpAttribute : NUnitAttribute
     { }
-
-    /// <summary>
-    /// Attribute used to mark a class that contains one-time SetUp 
-    /// and/or TearDown methods that apply to all the tests in a
-    /// namespace or an assembly.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited=true)]
-    public class PreTestAttribute : NUnitAttribute
-    { }
-
-    /// <summary>
-    /// Attribute used to mark a class that contains one-time SetUp 
-    /// and/or TearDown methods that apply to all the tests in a
-    /// namespace or an assembly.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited=true)]
-    public class PostTestAttribute : NUnitAttribute
-    { }
 }
