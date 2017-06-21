f0c5d50ca4041f12e360046457cc81ca688a601b
OmicronPersei <wheeler.weise@gmail.com>
2017-06-20 21:53:32 -0500

    Initial commit of ExactlyOneConstraint class.
    One unit test fails so far.

---
+++ src/NUnitFramework/framework/Constraints/ExactlyOneConstraint.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.Framework.Constraints
+{
+    /// <summary>
+    /// <see cref="ExactlyOneConstraint"/> applies another constraint to
+    /// a singular item in a collection, succeeding only if the singular 
+    /// item in the collection succeeds.
+    /// </summary>
+    public class ExactlyOneConstraint : Constraint
+    {
+        private readonly IConstraint _itemConstraint;
+        
+        /// <summary>
+        /// Construct a standalone <see cref="ExactlyOneConstraint"/>.
+        /// </summary>
+        public ExactlyOneConstraint()
+        { }
+
+        /// <summary>
+        /// Construct an <see cref="ExactlyOneConstraint"/> on top of
+        /// an existing constraint.
+        /// </summary>
+        /// <param name="itemConstraint"></param>
+        public ExactlyOneConstraint(IConstraint itemConstraint)
+            : base(itemConstraint)
+        {
+            Guard.ArgumentNotNull(itemConstraint, "itemConstraint");
+
+            _itemConstraint = itemConstraint;
+        }
+
+        /// <summary>
+        /// Apply the constraint to the singular item in the collection, 
+        /// succeeding only if the singular item passes.
+        /// </summary>
+        /// <typeparam name="TActual"></typeparam>
+        /// <param name="actual"></param>
+        /// <returns></returns>
+        public override ConstraintResult ApplyTo<TActual>(TActual actual)
+        {
+            if (!(actual is IEnumerable))
+                throw new ArgumentException("The constraint provided is not an IEnumerable", "actual");
+
+            IEnumerable actualEnumerable = (IEnumerable)actual;
+            object firstItem = actualEnumerable.Cast<object>().First();
+            bool firstItemIsSuccess = _itemConstraint.ApplyTo(firstItem).IsSuccess;
+
+            return new ConstraintResult(this, actual, firstItemIsSuccess);
+        }
+    }
+}
---
+++ src/NUnitFramework/tests/Constraints/ExactlyOneConstraintTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework.Constraints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.Framework.Tests.Constraints
+{
+    public class ExactlyOneConstraintTests
+    {
+        private static IEnumerable<string> testCollection = new List<string> { "item" };
+
+        [Test]
+        public void ExactlyOneItemMatches()
+        {
+            Assert.That(testCollection, new ExactlyOneConstraint(Is.EqualTo("item")));
+        }
+
+        [Test]
+        public void ExactlyOneItemDoesNotMatch()
+        {
+            Assert.That(testCollection, new ExactlyOneConstraint(Is.Not.EqualTo("notItem")));
+        }
+
+        [Test]
+        public void ExactlyOneThrowsWhenNotIEnumberable()
+        {
+            Assert.Throws<ArgumentException>(delegate ()
+            {
+                Assert.That(new int(), new ExactlyOneConstraint(Is.EqualTo("item")));
+            });
+        }
+
+        [Test]
+        public void ExactlyOneThrowsWhenConstraintIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var blah = new ExactlyOneConstraint(null);
+            });
+        }
+    }
+}
