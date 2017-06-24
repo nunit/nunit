18f37b334a12e5fe84c462edb9324e38e34f4fa9
OmicronPersei <wheeler.weise@gmail.com>
2017-06-24 11:33:27 -0500

    Bulk of logic for ExactlyOneConstraint finished.

--- src/NUnitFramework/framework/Constraints/ExactlyOneConstraint.cs
+++ src/NUnitFramework/framework/Constraints/ExactlyOneConstraint.cs
@@ -48,10 +48,25 @@ namespace NUnit.Framework.Constraints
                 throw new ArgumentException("The constraint provided is not an IEnumerable", "actual");
 
             IEnumerable actualEnumerable = (IEnumerable)actual;
-            object firstItem = actualEnumerable.Cast<object>().First();
-            bool firstItemIsSuccess = _itemConstraint.ApplyTo(firstItem).IsSuccess;
 
-            return new ConstraintResult(this, actual, firstItemIsSuccess);
+            int count = actualEnumerable.Cast<object>().Count();
+
+            if (count != 1)
+            {
+                return new ConstraintResult(this, actual, false);
+            }
+
+            if (_itemConstraint == null)
+            {
+                //Don't need to check the value of the singular item in the collection.
+                return new ConstraintResult(this, actual, true);
+            } else
+            {
+                object firstItem = actualEnumerable.Cast<object>().First();
+                bool firstItemIsSuccess = _itemConstraint.ApplyTo(firstItem).IsSuccess;
+
+                return new ConstraintResult(this, actual, firstItemIsSuccess);
+            }
         }
     }
 }
--- src/NUnitFramework/tests/Constraints/ExactlyOneConstraintTests.cs
+++ src/NUnitFramework/tests/Constraints/ExactlyOneConstraintTests.cs
@@ -9,18 +9,43 @@ namespace NUnit.Framework.Tests.Constraints
 {
     public class ExactlyOneConstraintTests
     {
-        private static IEnumerable<string> testCollection = new List<string> { "item" };
+        private static IEnumerable<string> testCollectionLen1 = new List<string> { "item" };
+        private static IEnumerable<string> testCollectionLen2 = new List<string> { "item", "otherItem" };
 
         [Test]
         public void ExactlyOneItemMatches()
         {
-            Assert.That(testCollection, new ExactlyOneConstraint(Is.EqualTo("item")));
+            Assert.That(testCollectionLen1, new ExactlyOneConstraint(Is.EqualTo("item")));
         }
 
         [Test]
         public void ExactlyOneItemDoesNotMatch()
         {
-            Assert.That(testCollection, new ExactlyOneConstraint(Is.Not.EqualTo("notItem")));
+            Assert.That(testCollectionLen1, new ExactlyOneConstraint(Is.Not.EqualTo("notItem")));
+        }
+
+        [Test]
+        public void ExactlyOneBlankConstraintWhereCollectionIsOne()
+        {
+            Assert.That(testCollectionLen1, new ExactlyOneConstraint());
+        }
+
+        [Test]
+        public void ExactlyOneBlankConstraintWhereCollectionIsTwo()
+        {
+            Assert.IsFalse(new ExactlyOneConstraint().ApplyTo(testCollectionLen2).IsSuccess);
+        }
+
+        [Test]
+        public void ExactlyOneConstraintMatchingWhereCollectionIsTwo()
+        {
+            Assert.IsFalse(new ExactlyOneConstraint(Is.EqualTo("item")).ApplyTo(testCollectionLen2).IsSuccess);
+        }
+
+        [Test]
+        public void ExactlyOneConstraintNotMatchingWhereCollectionIsTwo()
+        {
+            Assert.IsFalse(new ExactlyOneConstraint(Is.EqualTo("blah")).ApplyTo(testCollectionLen2).IsSuccess);
         }
 
         [Test]
