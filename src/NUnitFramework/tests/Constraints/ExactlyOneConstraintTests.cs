d357ff6b45689836728093858ebef872787fc31c
Rob Prouse <rob.prouse@bentley.com>
2017-08-25 14:18:52 -0400

    .NET 2.0 and 3.5 now building in the main solution

--- src/NUnitFramework/tests/Constraints/ExactlyOneConstraintTests.cs
+++ src/NUnitFramework/tests/Constraints/ExactlyOneConstraintTests.cs
@@ -8,10 +8,10 @@
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to
 // the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -21,13 +21,8 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // ***********************************************************************
 
-using NUnit.Framework.Constraints;
-using NUnit.Framework.Internal;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NUnit.Framework.Internal;
 
 namespace NUnit.Framework.Tests.Constraints
 {
