3dc0f5da1c15a1a36f80736b14616381f3b3b7d5
jnm2 <me@jnm2.com>
2017-11-29 20:50:23 -0500

    Added failing tests for comparison and equality adapters and nulls

---
+++ src/NUnitFramework/tests/Constraints/ComparisonAdapterTests.cs
@@ -0,0 +1,51 @@
+// ***********************************************************************
+// Copyright (c) 2017 Charlie Poole, Rob Prouse
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// ***********************************************************************
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace NUnit.Framework.Constraints
+{
+    public static class ComparisonAdapterTests
+    {
+        public static IEnumerable<ComparisonAdapter> ComparisonAdapters()
+        {
+            return new[]
+            {
+                ComparisonAdapter.Default,
+                ComparisonAdapter.For((IComparer)StringComparer.Ordinal),
+                ComparisonAdapter.For((IComparer<string>)StringComparer.Ordinal),
+                ComparisonAdapter.For<string>(StringComparer.Ordinal.Compare)
+            };
+        }
+
+        [TestCaseSource(nameof(ComparisonAdapters))]
+        public static void CanCompareWithNull(ComparisonAdapter adapter)
+        {
+            Assert.That(adapter.Compare(null, "a"), Is.LessThan(0));
+            Assert.That(adapter.Compare("a", null), Is.GreaterThan(0));
+            Assert.That(adapter.Compare(null, null), Is.Zero);
+        }
+    }
+}
