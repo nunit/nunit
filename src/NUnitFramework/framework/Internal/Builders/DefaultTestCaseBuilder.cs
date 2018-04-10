674e8f1025dfb40d985d3dc964655528308cf35d
Dai Michael <david.llewellyn.michael@gmail.com>
2018-04-10 19:31:00 +0100

    2759 - Improve function docs

--- src/NUnitFramework/framework/Internal/Builders/DefaultTestCaseBuilder.cs
+++ src/NUnitFramework/framework/Internal/Builders/DefaultTestCaseBuilder.cs
@@ -141,7 +141,7 @@ namespace NUnit.Framework.Internal.Builders
 
         /// <summary>
         /// Checks to see if the method is parameterized with DataAttribute, 
-        /// if the argument has 0 data it can fail the test when it should be not run / not exist.
+        /// if the argument has no data, it can fail the test when it should not be run.
         /// </summary>
         /// <param name="method">The method for which a test is to be built.</param>
         /// <returns>True if the method signature contains DataAttribute arguments</returns>
