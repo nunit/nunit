8476458750290de1465b34f37b17428ca2e265bf
Simon Bosanquet <40687840+sboptimal@users.noreply.github.com>
2018-08-12 18:21:55 +0100

    Correction to summary

--- src/NUnitFramework/framework/Attributes/RetryAttribute.cs
+++ src/NUnitFramework/framework/Attributes/RetryAttribute.cs
@@ -29,7 +29,7 @@ using NUnit.Framework.Internal.Commands;
 namespace NUnit.Framework
 {
     /// <summary>
-    /// <see cref="RetryAttribute" /> specified that a test method should
+    /// <see cref="RetryAttribute" /> specifies that a test method should
     /// be rerun if it fails, up to a maximum number of times.
     /// </summary>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
