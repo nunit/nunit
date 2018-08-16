fb7cd3444d006014d4882245d0c7a209ec890b5d
Nisha Kaushik <nishakaushik0@gmail.com>
2018-08-16 20:58:03 +0530

    Improve error message on EmptyConstraint

--- src/NUnitFramework/framework/Constraints/EmptyConstraint.cs
+++ src/NUnitFramework/framework/Constraints/EmptyConstraint.cs
@@ -21,7 +21,6 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // ***********************************************************************
 
-
 using System;
 
 namespace NUnit.Framework.Constraints
