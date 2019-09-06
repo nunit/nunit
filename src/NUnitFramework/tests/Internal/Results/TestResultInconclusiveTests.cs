c39456ddc79c004fdcf8121e76d87d063a8c1e38
ashishdawale20 <ashishdawale20@gmail.com>
2019-09-06 17:33:54 +0530

    fix for issue #3368

--- src/NUnitFramework/tests/Internal/Results/TestResultInconclusiveTests.cs
+++ src/NUnitFramework/tests/Internal/Results/TestResultInconclusiveTests.cs
@@ -84,7 +84,7 @@ namespace NUnit.Framework.Internal.Results
         {
             Assert.AreEqual(ResultState.Inconclusive, _suiteResult.ResultState);
             Assert.Null(_suiteResult.Message);
-
+            Assert.AreEqual(1, _suiteResult.TotalCount);
             Assert.AreEqual(0, _suiteResult.PassCount);
             Assert.AreEqual(0, _suiteResult.FailCount);
             Assert.AreEqual(0, _suiteResult.WarningCount);
--- src/NUnitFramework/tests/WorkItemTests.cs
+++ src/NUnitFramework/tests/WorkItemTests.cs
@@ -135,7 +135,7 @@ namespace NUnit.Framework.Internal.Execution
             {
                 Parent = new FakeTest("Fixture", fixtureApartment)
                 {
-                    Parent  = new FakeTest("Assembly", assemblyApartment)
+                    Parent = new FakeTest("Assembly", assemblyApartment)
                 }
             };
 
@@ -171,7 +171,7 @@ namespace NUnit.Framework.Internal.Execution
             public FakeTestResult(ITest test) : base(test)
             {
             }
-
+            public override int TotalCount => 1;
             public override int FailCount => 0;
 
             public override int WarningCount => 0;
