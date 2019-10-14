2d4432bbad57b3d73bff02dece4c738ce838af5a
stevenaw <sweerdenburg@gmail.com>
2019-10-14 17:00:11 -0400

    Remove comments

--- src/NUnitFramework/tests/Constraints/DictionaryContainsKeyConstraintTests.cs
+++ src/NUnitFramework/tests/Constraints/DictionaryContainsKeyConstraintTests.cs
@@ -449,7 +449,6 @@ namespace NUnit.Framework.Constraints
             public TestNonGenericDictionary(int key)
             {
                 _key = key;
-                //Keys = new[] { key };
             }
 
             public bool Contains(object key)
