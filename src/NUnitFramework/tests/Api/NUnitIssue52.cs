a41401b8eb10d477f991048668af0d57d79fec20
stevenaw <sweerdenburg@gmail.com>
2019-10-16 22:56:19 -0400

    writ a test for it

--- src/NUnitFramework/tests/Api/NUnitIssue52.cs
+++ src/NUnitFramework/tests/Api/NUnitIssue52.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using NUnit.Framework.Constraints;
 
 namespace NUnit.Framework.Api
 {
@@ -19,11 +21,18 @@ namespace NUnit.Framework.Api
             var item = new SelfContainer();
             var items = new SelfContainer[] { new SelfContainer(), item };
 
+
+
             // work around
             //Assert.True(((ICollection<SelfContainer>)items).Contains(item));
 
             // causes StackOverflowException
-            Assert.Contains(item, items);
+            //Assert.Contains(item, items);
+
+            var equalityComparer = new NUnitEqualityComparer();
+            var tolerance = Tolerance.Default;
+            var equal = equalityComparer.AreEqual(item, items, ref tolerance);
+            Assert.IsFalse(equal);
             //Console.WriteLine("test completed");
         }
     }
