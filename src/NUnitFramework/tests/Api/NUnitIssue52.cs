b28e3aa7d32db4d4e3e05d1f623c1b2bf9194272
stevenaw <sweerdenburg@gmail.com>
2019-10-19 17:11:41 -0400

    Add test explicitly checking recursion

--- src/NUnitFramework/tests/Api/NUnitIssue52.cs
+++ src/NUnitFramework/tests/Api/NUnitIssue52.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using NUnit.Framework.Constraints;
 
 namespace NUnit.Framework.Api
@@ -10,30 +11,67 @@ namespace NUnit.Framework.Api
     [TestFixture]
     public class NUnitIssue52
     {
-        class SelfContainer : IEnumerable
+        [TestCaseSource(nameof(GetTestCases))]
+        public void SelfContainedItemFoundInCollection<T>(T x, ICollection y)
         {
-            public IEnumerator GetEnumerator() { yield return this; }
+            var equalityComparer = new NUnitEqualityComparer();
+            var tolerance = Tolerance.Default;
+            var actualResult = equalityComparer.AreEqual(x, y, ref tolerance);
+
+            Assert.IsFalse(actualResult);
+            Assert.Contains(x, y);
         }
 
-        [Test]
-        public void SelfContainedItemFoundInArray()
+        [TestCaseSource(nameof(GetTestCases))]
+        public void SelfContainedItemDoesntRecurseForever<T>(T x, ICollection y)
+        {
+            var equalityComparer = new NUnitEqualityComparer();
+            var tolerance = Tolerance.Default;
+            equalityComparer.ExternalComparers.Add(new DetectRecursionComparer(30));
+
+            Assert.DoesNotThrow(() =>
+            {
+                var equality = equalityComparer.AreEqual(x, y, ref tolerance);
+                Assert.IsFalse(equality);
+                Assert.Contains(x, y);
+            });
+        }
+
+        public static IEnumerable<TestCaseData> GetTestCases()
         {
             var item = new SelfContainer();
             var items = new SelfContainer[] { new SelfContainer(), item };
 
+            yield return new TestCaseData(item, items);
+        }
 
+        private class DetectRecursionComparer : EqualityAdapter
+        {
+            private readonly int maxRecursion;
 
-            // work around
-            //Assert.True(((ICollection<SelfContainer>)items).Contains(item));
+            [MethodImpl(MethodImplOptions.NoInlining)]
+            public DetectRecursionComparer(int maxRecursion)
+            {
+                var callerDepth = new StackTrace().FrameCount - 1;
+                this.maxRecursion = callerDepth + maxRecursion;
+            }
 
-            // causes StackOverflowException
-            //Assert.Contains(item, items);
+            [MethodImpl(MethodImplOptions.NoInlining)]
+            public override bool CanCompare(object x, object y)
+            {
+                var currentDepth = new StackTrace().FrameCount - 1;
+                return currentDepth >= maxRecursion;
+            }
 
-            var equalityComparer = new NUnitEqualityComparer();
-            var tolerance = Tolerance.Default;
-            var equal = equalityComparer.AreEqual(item, items, ref tolerance);
-            Assert.IsFalse(equal);
-            //Console.WriteLine("test completed");
+            public override bool AreEqual(object x, object y)
+            {
+                throw new InvalidOperationException("Recurses");
+            }
+        }
+
+        private class SelfContainer : IEnumerable
+        {
+            public IEnumerator GetEnumerator() { yield return this; }
         }
     }
 }
