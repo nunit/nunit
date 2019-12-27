2e4ebf7d38e1e8e16689f06e63c4bed236e63af5
jnm2 <me@jnm2.com>
2019-12-26 23:09:11 -0500

    Annotate Assert class

--- src/NUnitFramework/framework/Assert.Types.cs
+++ src/NUnitFramework/framework/Assert.Types.cs
@@ -21,6 +21,8 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // ***********************************************************************
 
+#nullable enable
+
 using System;
 
 namespace NUnit.Framework
@@ -30,23 +32,25 @@ namespace NUnit.Framework
         #region IsAssignableFrom
 
         /// <summary>
-        /// Asserts that an object may be assigned a value of a given Type.
+        /// Asserts that an object may be assigned a value of a given Type. Returns without throwing an exception when
+        /// inside a multiple assert block.
         /// </summary>
         /// <param name="expected">The expected Type.</param>
         /// <param name="actual">The object under examination</param>
         /// <param name="message">The message to display in case of failure</param>
         /// <param name="args">Array of objects to be used in formatting the message</param>
-        public static void IsAssignableFrom(Type expected, object actual, string message, params object[] args)
+        public static void IsAssignableFrom(Type expected, object? actual, string? message, params object?[]? args)
         {
             Assert.That(actual, Is.AssignableFrom(expected) ,message, args);
         }
 
         /// <summary>
-        /// Asserts that an object may be assigned a value of a given Type.
+        /// Asserts that an object may be assigned a value of a given Type. Returns without throwing an exception when
+        /// inside a multiple assert block.
         /// </summary>
         /// <param name="expected">The expected Type.</param>
         /// <param name="actual">The object under examination</param>
-        public static void IsAssignableFrom(Type expected, object actual)
+        public static void IsAssignableFrom(Type expected, object? actual)
         {
             Assert.That(actual, Is.AssignableFrom(expected) ,null, null);
         }
@@ -56,23 +60,25 @@ namespace NUnit.Framework
         #region IsAssignableFrom<TExpected>
 
         /// <summary>
-        /// Asserts that an object may be assigned a value of a given Type.
+        /// Asserts that an object may be assigned a value of a given Type. Returns without throwing an exception when
+        /// inside a multiple assert block.
         /// </summary>
         /// <typeparam name="TExpected">The expected Type.</typeparam>
         /// <param name="actual">The object under examination</param>
         /// <param name="message">The message to display in case of failure</param>
         /// <param name="args">Array of objects to be used in formatting the message</param>
-        public static void IsAssignableFrom<TExpected>(object actual, string message, params object[] args)
+        public static void IsAssignableFrom<TExpected>(object? actual, string? message, params object?[]? args)
         {
             Assert.That(actual, Is.AssignableFrom(typeof(TExpected)) ,message, args);
         }
 
         /// <summary>
-        /// Asserts that an object may be assigned a value of a given Type.
+        /// Asserts that an object may be assigned a value of a given Type. Returns without throwing an exception when
+        /// inside a multiple assert block.
         /// </summary>
         /// <typeparam name="TExpected">The expected Type.</typeparam>
         /// <param name="actual">The object under examination</param>
-        public static void IsAssignableFrom<TExpected>(object actual)
+        public static void IsAssignableFrom<TExpected>(object? actual)
         {
             Assert.That(actual, Is.AssignableFrom(typeof(TExpected)) ,null, null);
         }
@@ -82,23 +88,25 @@ namespace NUnit.Framework
         #region IsNotAssignableFrom
 
         /// <summary>
-        /// Asserts that an object may not be assigned a value of a given Type.
+        /// Asserts that an object may not be assigned a value of a given Type. Returns without throwing an exception
+        /// when inside a multiple assert block.
         /// </summary>
         /// <param name="expected">The expected Type.</param>
         /// <param name="actual">The object under examination</param>
         /// <param name="message">The message to display in case of failure</param>
         /// <param name="args">Array of objects to be used in formatting the message</param>
-        public static void IsNotAssignableFrom(Type expected, object actual, string message, params object[] args)
+        public static void IsNotAssignableFrom(Type expected, object? actual, string? message, params object?[]? args)
         {
             Assert.That(actual, Is.Not.AssignableFrom(expected) ,message, args);
         }
 
         /// <summary>
-        /// Asserts that an object may not be assigned a value of a given Type.
+        /// Asserts that an object may not be assigned a value of a given Type. Returns without throwing an exception
+        /// when inside a multiple assert block.
         /// </summary>
         /// <param name="expected">The expected Type.</param>
         /// <param name="actual">The object under examination</param>
-        public static void IsNotAssignableFrom(Type expected, object actual)
+        public static void IsNotAssignableFrom(Type expected, object? actual)
         {
             Assert.That(actual, Is.Not.AssignableFrom(expected) ,null, null);
         }
@@ -108,23 +116,25 @@ namespace NUnit.Framework
         #region IsNotAssignableFrom<TExpected>
 
         /// <summary>
-        /// Asserts that an object may not be assigned a value of a given Type.
+        /// Asserts that an object may not be assigned a value of a given Type. Returns without throwing an exception
+        /// when inside a multiple assert block.
         /// </summary>
         /// <typeparam name="TExpected">The expected Type.</typeparam>
         /// <param name="actual">The object under examination</param>
         /// <param name="message">The message to display in case of failure</param>
         /// <param name="args">Array of objects to be used in formatting the message</param>
-        public static void IsNotAssignableFrom<TExpected>(object actual, string message, params object[] args)
+        public static void IsNotAssignableFrom<TExpected>(object? actual, string? message, params object?[]? args)
         {
             Assert.That(actual, Is.Not.AssignableFrom(typeof(TExpected)) ,message, args);
         }
 
         /// <summary>
-        /// Asserts that an object may not be assigned a value of a given Type.
+        /// Asserts that an object may not be assigned a value of a given Type. Returns without throwing an exception
+        /// when inside a multiple assert block.
         /// </summary>
         /// <typeparam name="TExpected">The expected Type.</typeparam>
         /// <param name="actual">The object under examination</param>
-        public static void IsNotAssignableFrom<TExpected>(object actual)
+        public static void IsNotAssignableFrom<TExpected>(object? actual)
         {
             Assert.That(actual, Is.Not.AssignableFrom(typeof(TExpected)) ,null, null);
         }
@@ -134,23 +144,25 @@ namespace NUnit.Framework
         #region IsInstanceOf
 
         /// <summary>
-        /// Asserts that an object is an instance of a given type.
+        /// Asserts that an object is an instance of a given type. Returns without throwing an exception when inside a
+        /// multiple assert block.
         /// </summary>
         /// <param name="expected">The expected Type</param>
         /// <param name="actual">The object being examined</param>
         /// <param name="message">The message to display in case of failure</param>
         /// <param name="args">Array of objects to be used in formatting the message</param>
-        public static void IsInstanceOf(Type expected, object actual, string message, params object[] args)
+        public static void IsInstanceOf(Type expected, object? actual, string? message, params object?[]? args)
         {
             Assert.That(actual, Is.InstanceOf(expected) ,message, args);
         }
 
         /// <summary>
-        /// Asserts that an object is an instance of a given type.
+        /// Asserts that an object is an instance of a given type. Returns without throwing an exception when inside a
+        /// multiple assert block.
         /// </summary>
         /// <param name="expected">The expected Type</param>
         /// <param name="actual">The object being examined</param>
-        public static void IsInstanceOf(Type expected, object actual)
+        public static void IsInstanceOf(Type expected, object? actual)
         {
             Assert.That(actual, Is.InstanceOf(expected) ,null, null);
         }
@@ -160,23 +172,25 @@ namespace NUnit.Framework
         #region IsInstanceOf<TExpected>
 
         /// <summary>
-        /// Asserts that an object is an instance of a given type.
+        /// Asserts that an object is an instance of a given type. Returns without throwing an exception when inside a
+        /// multiple assert block.
         /// </summary>
         /// <typeparam name="TExpected">The expected Type</typeparam>
         /// <param name="actual">The object being examined</param>
         /// <param name="message">The message to display in case of failure</param>
         /// <param name="args">Array of objects to be used in formatting the message</param>
-        public static void IsInstanceOf<TExpected>(object actual, string message, params object[] args)
+        public static void IsInstanceOf<TExpected>(object? actual, string? message, params object?[]? args)
         {
             Assert.That(actual, Is.InstanceOf(typeof(TExpected)) ,message, args);
         }
 
         /// <summary>
-        /// Asserts that an object is an instance of a given type.
+        /// Asserts that an object is an instance of a given type. Returns without throwing an exception when inside a
+        /// multiple assert block.
         /// </summary>
         /// <typeparam name="TExpected">The expected Type</typeparam>
         /// <param name="actual">The object being examined</param>
-        public static void IsInstanceOf<TExpected>(object actual)
+        public static void IsInstanceOf<TExpected>(object? actual)
         {
             Assert.That(actual, Is.InstanceOf(typeof(TExpected)) ,null, null);
         }
@@ -186,23 +200,25 @@ namespace NUnit.Framework
         #region IsNotInstanceOf
 
         /// <summary>
-        /// Asserts that an object is not an instance of a given type.
+        /// Asserts that an object is not an instance of a given type. Returns without throwing an exception when inside
+        /// a multiple assert block.
         /// </summary>
         /// <param name="expected">The expected Type</param>
         /// <param name="actual">The object being examined</param>
         /// <param name="message">The message to display in case of failure</param>
         /// <param name="args">Array of objects to be used in formatting the message</param>
-        public static void IsNotInstanceOf(Type expected, object actual, string message, params object[] args)
+        public static void IsNotInstanceOf(Type expected, object? actual, string? message, params object?[]? args)
         {
             Assert.That(actual, Is.Not.InstanceOf(expected) ,message, args);
         }
 
         /// <summary>
-        /// Asserts that an object is not an instance of a given type.
+        /// Asserts that an object is not an instance of a given type. Returns without throwing an exception when inside
+        /// a multiple assert block.
         /// </summary>
         /// <param name="expected">The expected Type</param>
         /// <param name="actual">The object being examined</param>
-        public static void IsNotInstanceOf(Type expected, object actual)
+        public static void IsNotInstanceOf(Type expected, object? actual)
         {
             Assert.That(actual, Is.Not.InstanceOf(expected) ,null, null);
         }
@@ -212,23 +228,25 @@ namespace NUnit.Framework
         #region IsNotInstanceOf<TExpected>
 
         /// <summary>
-        /// Asserts that an object is not an instance of a given type.
+        /// Asserts that an object is not an instance of a given type. Returns without throwing an exception when inside
+        /// a multiple assert block.
         /// </summary>
         /// <typeparam name="TExpected">The expected Type</typeparam>
         /// <param name="actual">The object being examined</param>
         /// <param name="message">The message to display in case of failure</param>
         /// <param name="args">Array of objects to be used in formatting the message</param>
-        public static void IsNotInstanceOf<TExpected>(object actual, string message, params object[] args)
+        public static void IsNotInstanceOf<TExpected>(object? actual, string? message, params object?[]? args)
         {
             Assert.That(actual, Is.Not.InstanceOf(typeof(TExpected)) ,message, args);
         }
 
         /// <summary>
-        /// Asserts that an object is not an instance of a given type.
+        /// Asserts that an object is not an instance of a given type. Returns without throwing an exception when inside
+        /// a multiple assert block.
         /// </summary>
         /// <typeparam name="TExpected">The expected Type</typeparam>
         /// <param name="actual">The object being examined</param>
-        public static void IsNotInstanceOf<TExpected>(object actual)
+        public static void IsNotInstanceOf<TExpected>(object? actual)
         {
             Assert.That(actual, Is.Not.InstanceOf(typeof(TExpected)) ,null, null);
         }
