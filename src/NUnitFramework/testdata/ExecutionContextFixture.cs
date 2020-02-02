5db383601a00dfd539ad0254416d7e01b9f5d206
jnm2 <me@jnm2.com>
2020-02-02 16:30:45 -0500

    Execution context should not be shared between test cases

---
+++ src/NUnitFramework/testdata/ExecutionContextFixture.cs
@@ -0,0 +1,53 @@
+// ***********************************************************************
+// Copyright (c) 2020 Charlie Poole, Rob Prouse
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// ***********************************************************************
+
+using NUnit.Framework;
+
+#if NET35 || NET40
+using System.Runtime.Remoting.Messaging;
+#else
+using System.Threading;
+#endif
+
+namespace NUnit.TestData
+{
+    public static class ExecutionContextFixture
+    {
+#if !(NET35 || NET40)
+        private static readonly AsyncLocal<bool> WasExecutionContextUsed = new AsyncLocal<bool>();
+#endif
+
+        public static void ExecutionContextIsNotSharedBetweenTestCases([Range(1, 2)] int testCase)
+        {
+#if NET35 || NET40
+            Assert.Null(CallContext.LogicalGetData("WasUsed"));
+
+            CallContext.LogicalSetData("WasUsed", true);
+#else
+            Assert.That(!WasExecutionContextUsed.Value);
+
+            WasExecutionContextUsed.Value = true;
+#endif
+        }
+    }
+}
--- src/NUnitFramework/tests/SynchronizationContextTests.cs
+++ src/NUnitFramework/tests/SynchronizationContextTests.cs
@@ -104,7 +104,7 @@ namespace NUnit.Framework
             });
         }
 
-#if NET40 || NET45
+#if NETFRAMEWORK
         // TODO: test a custom awaitable type whose awaiter executes continuations on a brand new thread
         // to ensure that the message pump is shut down on the correct thread.
 
