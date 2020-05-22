7a190e5c1a8e4dfab87dcac198f33d58a0d0a74e
Bastian Schmidt <batzen@gmx.org>
2020-05-22 18:26:50 +0200

    Simplifying code and using established boolean comparisons in csproj

--- src/NUnitFramework/tests/SynchronizationContextTests.cs
+++ src/NUnitFramework/tests/SynchronizationContextTests.cs
@@ -104,7 +104,7 @@ namespace NUnit.Framework
             });
         }
 
-#if UseWindowsFormsAndWPF || NETFRAMEWORK
+#if UseWindowsFormsAndWPF
         // TODO: test a custom awaitable type whose awaiter executes continuations on a brand new thread
         // to ensure that the message pump is shut down on the correct thread.
 
