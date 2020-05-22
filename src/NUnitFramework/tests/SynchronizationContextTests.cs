0d19fa0c83b02c5af0ceb1beccaadac898bccc92
Bastian Schmidt <batzen@gmx.org>
2020-05-22 15:59:01 +0200

    Enabling usage of platform specific sdk for winforms and wpf

--- src/NUnitFramework/tests/SynchronizationContextTests.cs
+++ src/NUnitFramework/tests/SynchronizationContextTests.cs
@@ -104,7 +104,7 @@ namespace NUnit.Framework
             });
         }
 
-#if !NETCOREAPP2_1
+#if UseWindowsFormsAndWPF || NETFRAMEWORK
         // TODO: test a custom awaitable type whose awaiter executes continuations on a brand new thread
         // to ensure that the message pump is shut down on the correct thread.
 
