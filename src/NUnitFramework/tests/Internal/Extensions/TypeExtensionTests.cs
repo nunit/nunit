2129ad621a7ef11edd7c01ee07b8a0721d0be854
Rob Prouse <rob@prouse.org>
2021-04-21 12:59:58 -0400

    Performance improvements comparing equivalence of large collections

---
+++ src/NUnitFramework/tests/Internal/Extensions/TypeExtensionTests.cs
@@ -0,0 +1,58 @@
+// Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+
+namespace NUnit.Framework.Internal.Extensions
+{
+    public class TypeExtensionTests
+    {
+        [TestCaseSource(nameof(TypesThatImplementIComparable))]
+        public void TypesThatImplementIComparable_ReturnTrue(Type type)
+        {
+            Assert.That(type.ImplementsIComparable(), Is.True);
+        }
+
+        [TestCaseSource(nameof(TypesThatDontImplementIComparable))]
+        public void TypesThatDontImplementIComparable_ReturnFalse(Type type)
+        {
+            Assert.That(type.ImplementsIComparable(), Is.False);
+        }
+
+        public static IEnumerable<Type> TypesThatImplementIComparable => new[]
+        {
+            typeof(byte),
+            typeof(sbyte),
+            typeof(int),
+            typeof(uint),
+            typeof(long),
+            typeof(ulong),
+            typeof(short),
+            typeof(ushort),
+            typeof(char),
+            typeof(float),
+            typeof(double),
+            typeof(decimal),
+            typeof(string),
+            typeof(bool),
+            typeof(DateTime),
+            typeof(DateTimeOffset),
+            typeof(TimeSpan),
+            typeof(Guid),
+            typeof(BigInteger),
+            typeof(ValueTuple),
+            typeof(Version),
+        };
+
+        public static IEnumerable<Type> TypesThatDontImplementIComparable => new[]
+        {
+            null,
+            typeof(object),
+            typeof(Exception),
+            typeof(ValueType),
+            typeof(Stream)
+        };
+    }
+}
