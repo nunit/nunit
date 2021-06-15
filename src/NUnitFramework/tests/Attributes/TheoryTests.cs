f000ccec67d33e51513334986929c0b845945092
Felix Kröner <kroener@dynamic-biosensors.com>
2021-06-15 08:20:19 +0200

    Add tests for nested theory searching in declaring type
    As suggested by mikkelbu

--- src/NUnitFramework/tests/Attributes/TheoryTests.cs
+++ src/NUnitFramework/tests/Attributes/TheoryTests.cs
@@ -175,6 +175,98 @@ namespace NUnit.Framework.Attributes
             }
         }
 
+        
+        public class NestedTheoryThatSearchesInDeclaringTypes
+        {
+            static readonly Type nestedType = typeof(TheoryFixture.NestedWhileSearchingInDeclaringType);
+
+            [Test]
+            public void WithNoArgumentsIsTreatedAsTest()
+            {
+                TestAssert.IsRunnable(nestedType, nameof(TheoryFixture.NestedWhileSearchingInDeclaringType.WithNoArguments));
+            }
+
+            [Test]
+            public void WithNoDatapointsIsRunnableButFails()
+            {
+                TestAssert.IsRunnable(nestedType, nameof(TheoryFixture.NestedWhileSearchingInDeclaringType.WithArgumentsButNoDatapoints), ResultState.Failure);
+            }
+
+            [Test]
+            public void WithUnsupportedNullableTypeArgumentWithNoDatapointsIsRunnableButFails()
+            {
+                TestAssert.IsRunnable(nestedType, nameof(TheoryFixture.NestedWhileSearchingInDeclaringType.WithUnsupportedNullableTypeArgumentWithNoDataPoints), ResultState.Failure);
+            }
+
+            [Test]
+            public void WithDatapointsIsRunnable()
+            {
+                Test test = TestBuilder.MakeParameterizedMethodSuite(nestedType, nameof(TheoryFixture.NestedWhileSearchingInDeclaringType.WithArgumentsAndDatapoints));
+                TestAssert.IsRunnable(test);
+                Assert.That(test.TestCaseCount, Is.EqualTo(9));
+            }
+
+            [Test]
+            public void WithBooleanArgumentsHasValuesSuppliedAutomatically()
+            {
+                Test test = TestBuilder.MakeParameterizedMethodSuite(nestedType, nameof(TheoryFixture.NestedWhileSearchingInDeclaringType.WithBooleanArguments));
+                TestAssert.IsRunnable(test);
+                Assert.That(test.TestCaseCount, Is.EqualTo(4));
+            }
+
+            [Test]
+            public void WithNullableBooleanArgumentsHasValuesSuppliedAutomatically()
+            {
+                Test test = TestBuilder.MakeParameterizedMethodSuite(nestedType, nameof(TheoryFixture.NestedWhileSearchingInDeclaringType.WithNullableBooleanArguments));
+                TestAssert.IsRunnable(test);
+                Assert.That(test.TestCaseCount, Is.EqualTo(9));
+            }
+
+            [Test]
+            public void WithDatapointAndAttributeDataIsRunnable()
+            {
+                Test test = TestBuilder.MakeParameterizedMethodSuite(nestedType, nameof(TheoryFixture.NestedWhileSearchingInDeclaringType.WithBothDatapointAndAttributeData));
+                TestAssert.IsRunnable(test);
+                Assert.That(test.TestCaseCount, Is.EqualTo(6));
+            }
+
+            [Test]
+            public void WithEnumArgumentsHasValuesSuppliedAutomatically()
+            {
+                Test test = TestBuilder.MakeParameterizedMethodSuite(nestedType, nameof(TheoryFixture.NestedWhileSearchingInDeclaringType.WithEnumAsArgument));
+                TestAssert.IsRunnable(test);
+                Assert.That(test.TestCaseCount, Is.EqualTo(16));
+            }
+
+            [Test]
+            public void WithNullableEnumArgumentsHasValuesSuppliedAutomatically()
+            {
+                Test test = TestBuilder.MakeParameterizedMethodSuite(nestedType, nameof(TheoryFixture.NestedWhileSearchingInDeclaringType.WithNullableEnumAsArgument));
+                TestAssert.IsRunnable(test);
+                Assert.That(test.TestCaseCount, Is.EqualTo(17));
+            }
+
+            [Test]
+            public void WithAllValuesSuppliedByAttributesIsRunnable()
+            {
+                // NOTE: This test was failing with a count of 8 because both
+                // TheoryAttribute and CombinatorialAttribute were adding cases.
+                // Solution is to make TheoryAttribute a CombiningAttribute so
+                // that no extra attribute is added to the method.
+                Test test = TestBuilder.MakeParameterizedMethodSuite(nestedType, nameof(TheoryFixture.NestedWhileSearchingInDeclaringType.WithAllDataSuppliedByAttributes));
+                TestAssert.IsRunnable(test);
+                Assert.That(test.TestCaseCount, Is.EqualTo(4));
+            }
+
+            [Test]
+            public void FailsIfAllTestsAreInconclusive()
+            {
+                ITestResult result = TestBuilder.RunParameterizedMethodSuite(nestedType, nameof(TheoryFixture.NestedWhileSearchingInDeclaringType.WithAllBadValues));
+                Assert.That(result.ResultState, Is.EqualTo(ResultState.Failure));
+                Assert.That(result.Message, Is.EqualTo("All test cases were inconclusive"));
+            }
+        }
+
         public class NestedSqrtTests
         {
             [Datapoint]
