b41111a642a5cc41cf1081df98a67a9efe7b540f
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-11 21:52:36 +0800

    Delete src directory

--- src/NUnitFramework/nunitlite-runner/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
-
-using System.Reflection;
-
-[assembly: AssemblyTitle("NUnit Test Runner")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCulture("")]
--- src/NUnitFramework/nunitlite/IDefaultOptionsProvider.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
-
-namespace NUnit.Common
-{
-    internal interface IDefaultOptionsProvider
-    {
-        bool TeamCity { get; }
-    }
-}
