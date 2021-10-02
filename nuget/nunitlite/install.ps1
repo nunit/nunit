a8dfaf64475bc4565512b2dba64523cd69917a02
Mikkel Nylander Bundgaard <mikkelbu@users.noreply.github.com>
2021-10-02 23:35:53 +0200

    fix: Remove install script and entry classes

--- nuget/nunitlite/install.ps1
+++
@@ -1,9 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-If ($project.Type -ne "VB.NET")
-{
-    $project.ProjectItems.Item("Program.vb").Delete()
-}
-If ($project.Type -ne "C#")
-{
-    $project.ProjectItems.Item("Program.cs").Delete()
-}
