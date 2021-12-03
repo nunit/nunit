2ea5914f523392a7e933bfc3a4c399269cc08f28
Louis Zanella <louis.zanella@gmail.com>
2021-12-03 15:29:55 -0500

    Make DictionaryContainsKeyValuePairConstraint support generic IDictionary
    - Adapt DictionaryContainsKeyValuePairConstraint so that it works with types that implement
      - IDictionary<TKey, TValue> but
      - not IDictionary
    - For backward compatibility, keep throwing same exceptions with same messages when failures arise

--- src/NUnitFramework/framework/Constraints/DictionaryContainsKeyValuePairConstraint.cs
+++ src/NUnitFramework/framework/Constraints/DictionaryContainsKeyValuePairConstraint.cs
@@ -21,7 +21,10 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // ***********************************************************************
 
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework.Internal;
 
 namespace NUnit.Framework.Constraints
@@ -61,12 +64,34 @@ namespace NUnit.Framework.Constraints
 
         private bool Matches(object actual)
         {
-            var dictionary = ConstraintUtils.RequireActual<IDictionary>(actual, nameof(actual));
-            foreach (var entry in dictionary)
-                if (ItemsEqual(entry, _expected))
-                    return true;
+            if (actual == null)
+                throw new ArgumentException("Expected: IDictionary But was: null", nameof(actual));
 
-            return false;
+            if (TypeHelper.TryCast<IDictionary>(actual, out var dictionary))
+            {
+                foreach (var entry in dictionary)
+                    if (ItemsEqual(entry, _expected))
+                        return true;
+                return false;
+            }
+
+            // If 'actual' implements IDictionary<TKey, TValue>, construct an 'expected' KeyValuePair<TKey, TValue>
+            // and look it up by iterating using IEnumerable
+            if (actual.GetType().GetInterfaces().Any(i => i.FullName.StartsWith("System.Collections.Generic.IDictionary`2[")))
+            {
+                var expected = new KeyValuePair<object, object>(_expected.Key, _expected.Value);
+                var enumerable = actual as IEnumerable;
+
+                foreach (var item in enumerable)
+                {
+                    if (ItemsEqual(item, expected))
+                        return true;
+                }
+
+                return false;
+            }
+
+            throw new ArgumentException($"Expected: IDictionary But was: {actual.GetType()}", nameof(actual));
         }
 
         /// <summary>
