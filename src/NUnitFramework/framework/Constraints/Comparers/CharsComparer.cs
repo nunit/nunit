856f2b576aba46837add61af4b66192d2d4de3f7
Simon Cropp <simon.cropp@gmail.com>
2022-10-08 18:43:00 +1100

    .

--- src/NUnitFramework/framework/Constraints/Comparers/CharsComparer.cs
+++ src/NUnitFramework/framework/Constraints/Comparers/CharsComparer.cs
@@ -1,5 +1,6 @@
 // Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
 
+#nullable enable
 using System;
 
 namespace NUnit.Framework.Constraints.Comparers
@@ -11,13 +12,13 @@ namespace NUnit.Framework.Constraints.Comparers
     {
         public static bool? Equal(object x, object y, ref Tolerance tolerance, ComparisonState state, NUnitEqualityComparer equalityComparer)
         {
-            if (!(x is char xChar) || !(y is char yChar))
+            if (x is not char xChar || y is not char yChar)
                 return null;
 
             bool caseInsensitive = equalityComparer.IgnoreCase;
 
-            char c1 = caseInsensitive ? Char.ToLower(xChar) : xChar;
-            char c2 = caseInsensitive ? Char.ToLower(yChar) : yChar;
+            char c1 = caseInsensitive ? char.ToLower(xChar) : xChar;
+            char c2 = caseInsensitive ? char.ToLower(yChar) : yChar;
 
             return c1 == c2;
         }
