2a501e9adfe2b0bfdaa332da1612a41d32c8f184
Simon Cropp <simon.cropp@gmail.com>
2022-10-30 22:07:54 +1100

    .

--- src/NUnitFramework/tests/GlobalUsings.cs
+++ src/NUnitFramework/tests/GlobalUsings.cs
@@ -1,21 +1,10 @@
 ﻿// Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
 
-global using System.Collections;
-global using System.Collections.Concurrent;
-global using System.Diagnostics;
-global using System.Globalization;
 global using NUnit.Framework.Interfaces;
 global using NUnit.Framework.Internal;
-global using System.Runtime.Versioning;
-global using System.Web.UI;
-global using System.Xml.Linq;
 global using NUnit.Framework.Internal.Filters;
 global using NUnit.TestUtilities;
 global using NUnit.Tests.Assemblies;
-global using System.Reflection;
-global using System.Security.Principal;
-global using System.Runtime.Serialization;
-global using System.Text;
 global using NUnit.Compatibility;
 global using NUnit.Framework;
 global using NUnit.Framework.Internal.Abstractions;
@@ -35,6 +24,18 @@ global using NUnit.TestData.RandomAttributeTests;
 global using NUnit.TestData.TestContextData;
 global using NUnit.TestUtilities.Collections;
 global using NUnit.TestUtilities.Comparers;
+global using System.Collections;
+global using System.Collections.Concurrent;
+global using System.Diagnostics;
+global using System.Globalization;
+global using System.Runtime.Versioning;
+global using System.Web.UI;
+global using System.Xml.Linq;
+global using System.Reflection;
+global using System.Security.Principal;
+global using System.Runtime.Serialization;
+global using System.Text;
+global using System.Collections.Immutable;
 #if NETFRAMEWORK
 global using System.Runtime.Remoting.Messaging;
 #endif
