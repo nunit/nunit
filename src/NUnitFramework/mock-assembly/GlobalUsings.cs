f643c7840ad35a62a33094225f8d3545e76524f3
Simon Cropp <simon.cropp@gmail.com>
2022-10-30 20:21:52 +1100

    .

---
+++ src/NUnitFramework/mock-assembly/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿// Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
+
+global using System.Reflection;
+global using NUnit.Framework;
+global using NUnit.Framework.Internal;
+global using NUnitLite;
--- src/NUnitFramework/mock-assembly/Singletons.cs
+++ src/NUnitFramework/mock-assembly/Singletons.cs
@@ -1,10 +1,5 @@
 // Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
 
-using System.Reflection;
-using NUnit.Framework;
-using NUnit.Framework.Internal;
-using NUnitLite;
-
 namespace NUnit.Tests.Singletons
 {
     [TestFixture]
