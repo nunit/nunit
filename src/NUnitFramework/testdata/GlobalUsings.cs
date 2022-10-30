22fe0af79a3d63a5a59472c2d77f7008cb241051
Simon Cropp <simon.cropp@gmail.com>
2022-10-30 22:17:47 +1100

    .

--- src/NUnitFramework/testdata/GlobalUsings.cs
+++ src/NUnitFramework/testdata/GlobalUsings.cs
@@ -1,3 +1,6 @@
 ﻿// Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
 
+global using System.Security.Principal;
 global using NUnit.Framework;
+global using System.Collections;
+global using NUnit.Framework.Interfaces;
