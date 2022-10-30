18079cf8bb538db5286e07092b02cc903d2f124e
Simon Cropp <simon.cropp@gmail.com>
2022-10-30 22:50:30 +1100

    .

--- src/NUnitFramework/nunitlite/TextRunner.cs
+++ src/NUnitFramework/nunitlite/TextRunner.cs
@@ -1,8 +1,6 @@
 // Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
 
 using System.Diagnostics;
-using NUnit;
-using NUnit.Framework.Internal.Filters;
 
 namespace NUnitLite
 {
