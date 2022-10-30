7ca6142878fb3deccfa0617026972baa8c9877db
Simon Cropp <simon.cropp@gmail.com>
2022-10-30 22:20:12 +1100

    .

--- src/NUnitFramework/framework/GlobalUsings.cs
+++ src/NUnitFramework/framework/GlobalUsings.cs
@@ -2,6 +2,7 @@
 
 global using System.Collections;
 global using System.Collections.Concurrent;
+global using System.Collections.ObjectModel;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Globalization;
 global using System.Reflection;
