fef181e731648f4b25ee09a03b127149931dcc29
Michael Render <render.michael@gmail.com>
2023-07-26 11:02:23 -0400

    fix file header

--- src/NUnitFramework/framework/Compatibility/StringSyntaxAttribute.cs
+++ src/NUnitFramework/framework/Compatibility/StringSyntaxAttribute.cs
@@ -1,7 +1,6 @@
-#if !NET7_0_OR_GREATER
+// Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
 
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
+#if !NET7_0_OR_GREATER
 
 namespace System.Diagnostics.CodeAnalysis
 {
