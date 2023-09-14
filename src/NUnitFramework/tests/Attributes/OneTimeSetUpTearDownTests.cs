bebd11898114aaaf46e6d769fb010fa25598e847
Manfred Brands <manfred-brands@users.noreply.github.com>
2023-09-14 20:29:06 +0800

    Issue2466 propagate stack trace to child result (#4467)
    * Ensure StackTrace of OneTimeSetUp is passed to child test results

--- src/NUnitFramework/tests/Attributes/OneTimeSetUpTearDownTests.cs
+++ src/NUnitFramework/tests/Attributes/OneTimeSetUpTearDownTests.cs
@@ -127,6 +127,28 @@ namespace NUnit.Framework.Tests.Attributes
         }
 
         [Test]
+        public void FailedSetUpStacktracePropogatesToTestResult()
+        {
+            SetUpAndTearDownFixture fixture = new SetUpAndTearDownFixture();
+            fixture.ThrowInBaseSetUp = true;
+            ITestResult result = TestBuilder.RunTestFixture(fixture);
+
+            Assert.That(result.ResultState.Site, Is.EqualTo(FailureSite.SetUp));
+            Assert.That(result.StackTrace, Is.Not.Null);
+            Assert.That(result.StackTrace, Does.Contain($"{nameof(SetUpAndTearDownFixture)}.{nameof(SetUpAndTearDownFixture.Init)}"));
+
+            Assert.That(result.HasChildren, Is.True);
+            foreach (var childResult in result.Children)
+            {
+                Assert.That(childResult.ResultState.Site, Is.EqualTo(FailureSite.Parent));
+                Assert.That(childResult.StackTrace, Is.EqualTo(result.StackTrace));
+            }
+
+            Assert.That(fixture.SetUpCount, Is.EqualTo(1));
+            Assert.That(fixture.TearDownCount, Is.EqualTo(1));
+        }
+
+        [Test]
         public void StaticBaseSetUpCalledFirstAndTearDownCalledLast()
         {
             StaticSetUpAndTearDownFixture.SetUpCount = 0;
