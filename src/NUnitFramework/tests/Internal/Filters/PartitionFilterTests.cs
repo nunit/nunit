1786b02eeafe9e5d57ca4ba608e2e6de7d91adf5
stevenaw <sweerdenburg@gmail.com>
2023-10-16 20:54:47 -0400

    Remove unused using

--- src/NUnitFramework/tests/Internal/Filters/PartitionFilterTests.cs
+++ src/NUnitFramework/tests/Internal/Filters/PartitionFilterTests.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
 
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework.Interfaces;
 using NUnit.Framework.Internal;
