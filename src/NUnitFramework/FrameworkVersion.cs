c0e3970a50308076ab4dec348ccf70247d1e876e
Terje Sandstrom <terje@hermit.no>
2024-05-07 21:38:47 +0200

    Fixes Issue #4705

--- src/NUnitFramework/FrameworkVersion.cs
+++ src/NUnitFramework/FrameworkVersion.cs
@@ -1,9 +1,9 @@
 // Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
 
-using System.Reflection;
+//using System.Reflection;
 
 //
 // Current version for the NUnit Framework
 //
-[assembly: AssemblyVersion("4.0.1.0")]
-[assembly: AssemblyFileVersion("4.0.1.0")]
+//[assembly: AssemblyVersion("4.0.1.0")]
+//[assembly: AssemblyFileVersion("4.0.1.0")]
