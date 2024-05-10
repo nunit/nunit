54a699d32ae60e9f22ec24563b307a571ddde3f1
Terje Sandstrom <terje@hermit.no>
2024-05-10 19:18:48 +0200

    Changed to using conditionals in the directory.build.props and adjusted csprojs and assemblyinfo.cs files accordingly.

--- src/NUnitFramework/tests/Properties/AssemblyInfo.cs
+++ src/NUnitFramework/tests/Properties/AssemblyInfo.cs
@@ -1,13 +1,6 @@
 // Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
 
-using System.Reflection;
-
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
-
-[assembly: AssemblyTitle("NUnit Framework Tests")]
-
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: NUnit.Framework.Parallelizable(NUnit.Framework.ParallelScope.Fixtures)]
