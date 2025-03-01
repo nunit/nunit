0102dd23911d5ff67830654d9648309935221266
Manfred Brands <manfred-brands@users.noreply.github.com>
2025-03-02 07:25:59 +0800

    Add Support for MultipleOf, Even and Odd. (#4946)
    * Add Support for MultipleOf, Even and Odd.
    
    * Added test to verify that 0 is considered even and not odd.
    
    * Remove questioning comment.
    
    Behaviour agreed upon.
    
    * Fix XML doc comment

---
+++ src/NUnitFramework/framework/Constraints/MultipleOfConstraint.cs
@@ -0,0 +1,54 @@
+// Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
+
+using System;
+
+namespace NUnit.Framework.Constraints
+{
+    /// <summary>
+    /// MultipleOfConstraint tests that the actual value is a multiple of a specified integer.
+    /// </summary>
+    public class MultipleOfConstraint : Constraint
+    {
+        private readonly uint _multiple;
+
+        /// <summary>
+        /// Initializes an instance of the <see cref="MultipleOfConstraint"/>.
+        /// </summary>
+        /// <param name="multiple">An integer value greater than zero</param>
+        public MultipleOfConstraint(int multiple)
+        {
+            Guard.ArgumentInRange(multiple > 0, "Multiple must be greater than zero", nameof(multiple));
+            _multiple = (uint)multiple;
+        }
+
+        /// <inheritdoc/>
+        public override string Description => $"MultipleOf({_multiple})";
+
+        /// <summary>
+        /// Test whether the constraint is satisfied by a given value
+        /// </summary>
+        /// <param name="actual">The value to be tested</param>
+        /// <returns>True for success, false for failure</returns>
+        public override ConstraintResult ApplyTo<TActual>(TActual actual)
+        {
+            bool result;
+
+            if (actual is int || actual is long || actual is short || actual is byte)
+            {
+                long value = Convert.ToInt64(actual);
+                result = value % _multiple == 0;
+            }
+            else if (actual is uint || actual is ulong || actual is ushort || actual is byte)
+            {
+                ulong value = Convert.ToUInt64(actual);
+                result = value % _multiple == 0;
+            }
+            else
+            {
+                result = false;
+            }
+
+            return new ConstraintResult(this, actual, result);
+        }
+    }
+}
---
+++ src/NUnitFramework/tests/Assertions/AssertMultipleOfTests.cs
@@ -0,0 +1,85 @@
+// Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
+
+namespace NUnit.Framework.Tests.Assertions
+{
+    [TestFixture]
+    public class AssertMultipleOfTests
+    {
+        private const int Number = 3;
+
+        private readonly byte _b1 = 3;
+        private readonly byte _b2 = 4;
+        private readonly int _i1 = 6;
+        private readonly int _i2 = 7;
+        private readonly uint _u1 = 9;
+        private readonly uint _u2 = 10;
+        private readonly long _l1 = 12;
+        private readonly long _l2 = 13;
+        private readonly ulong _ul1 = 15;
+        private readonly ulong _ul2 = 16;
+        private readonly float _f1 = 18F;
+        private readonly float _f2 = 19F;
+        private readonly decimal _de1 = 21M;
+        private readonly decimal _de2 = 22M;
+        private readonly double _d1 = 24D;
+        private readonly double _d2 = 25D;
+
+        [Test]
+        public void SucceedsIfMultipleOfNumber()
+        {
+            Assert.That(_b1, Is.MultipleOf(Number));
+            Assert.That(_i1, Is.MultipleOf(Number));
+            Assert.That(_u1, Is.MultipleOf(Number));
+            Assert.That(_l1, Is.MultipleOf(Number));
+            Assert.That(_ul1, Is.MultipleOf(Number));
+        }
+
+        [Test]
+        public void SucceedsIfOddOrEven()
+        {
+            Assert.That(_b1, Is.Odd);
+            Assert.That(_i1, Is.Even);
+            Assert.That(_u1, Is.Odd);
+            Assert.That(_l1, Is.Even);
+            Assert.That(_ul1, Is.Odd);
+        }
+
+        [Test]
+        public void FailsIfMultipleOfFloatingPoint()
+        {
+            // Should this throw an exception instead?
+            Assert.Throws<AssertionException>(() => Assert.That(_f1, Is.MultipleOf(Number)));
+            Assert.Throws<AssertionException>(() => Assert.That(_de1, Is.MultipleOf(Number)));
+            Assert.Throws<AssertionException>(() => Assert.That(_d1, Is.MultipleOf(Number)));
+
+            Assert.Throws<AssertionException>(() => Assert.That(_f2, Is.MultipleOf(Number)));
+            Assert.Throws<AssertionException>(() => Assert.That(_de2, Is.MultipleOf(Number)));
+            Assert.Throws<AssertionException>(() => Assert.That(_d2, Is.MultipleOf(Number)));
+        }
+
+        [Test]
+        public void FailsIfNotMultipleOfNumber()
+        {
+            Assert.Throws<AssertionException>(() => Assert.That(_b2, Is.MultipleOf(Number)));
+            Assert.Throws<AssertionException>(() => Assert.That(_i2, Is.MultipleOf(Number)));
+            Assert.Throws<AssertionException>(() => Assert.That(_u2, Is.MultipleOf(Number)));
+            Assert.Throws<AssertionException>(() => Assert.That(_l2, Is.MultipleOf(Number)));
+            Assert.Throws<AssertionException>(() => Assert.That(_ul2, Is.MultipleOf(Number)));
+        }
+
+        [Test]
+        public void Combines()
+        {
+            Assert.That(2 * 3, Is.MultipleOf(3).And.Even);
+            Assert.That(3 * 3, Is.MultipleOf(3).And.Odd);
+            Assert.That(2 * 4, Is.Not.MultipleOf(3).And.Even);
+        }
+
+        [Test]
+        public void ZeroIsEvenNotOdd()
+        {
+            Assert.That(0, Is.Even);
+            Assert.That(0, Is.Not.Odd);
+        }
+    }
+}
