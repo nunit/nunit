4d27ff0fdf5636cd65fd7357ce2c5fda1ec480cd
Stefan Vogt <stefan.sv.vogt@siemens-healthineers.com>
2025-06-11 11:14:27 +0200

    Use IWarpTestMethod to get rid of the need to change the MakeTestCommand method. This is not working on class level because it is only working on one of the tests. => ExecutionProceedsAfterBothTestHookCompletes test fails.

--- src/NUnitFramework/framework/Internal/Commands/HookDelegatingTestCommand.cs
+++ src/NUnitFramework/framework/Internal/Commands/HookDelegatingTestCommand.cs
@@ -4,7 +4,7 @@ using System;
 
 namespace NUnit.Framework.Internal.Commands
 {
-    internal class HookDelegatingTestCommand : DelegatingTestCommand
+    public class HookDelegatingTestCommand : DelegatingTestCommand
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="HookDelegatingTestCommand"/> class.
