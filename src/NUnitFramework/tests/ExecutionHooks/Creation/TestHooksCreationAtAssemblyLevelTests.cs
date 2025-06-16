8bb0932d914509e8ea957bbb4cea7110275ea785
Rajiv Sinha <rajiv.sinha@siemens-healthineers.com>
2025-06-16 10:55:45 +0200

    Fixed namespace

--- src/NUnitFramework/tests/ExecutionHooks/Creation/TestHooksCreationAtAssemblyLevelTests.cs
+++ src/NUnitFramework/tests/ExecutionHooks/Creation/TestHooksCreationAtAssemblyLevelTests.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
 
 using System;
+using NUnit.Framework.Attributes;
 using NUnit.Framework.Interfaces;
 using NUnit.Framework.Internal;
 using NUnit.Framework.Tests.TestUtilities;
