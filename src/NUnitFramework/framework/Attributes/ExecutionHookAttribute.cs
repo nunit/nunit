9ea47e03d20b1ca7e92eed8c44ef93a70dbffbf6
Manfred Brands <manfred-brands@users.noreply.github.com>
2025-06-19 17:33:35 +0800

    Remove need for Reflection to check if a method has an override.

--- src/NUnitFramework/framework/Attributes/ExecutionHookAttribute.cs
+++ src/NUnitFramework/framework/Attributes/ExecutionHookAttribute.cs
@@ -7,9 +7,39 @@ using NUnit.Framework.Internal.Commands;
 namespace NUnit.Framework
 {
     /// <summary>
+    /// Abstract base attribute class for defining execution hooks methods.
+    /// </summary>
+    public abstract class ExecutionHookMethodsAttribute : NUnitAttribute
+    {
+        /// <summary>
+        /// Method that is called <b>immediately before</b> the test is executed.
+        /// Override this to implement custom logic to run before the test.
+        /// </summary>
+        /// <param name="context">The current <see cref="TestExecutionContext"/> for the test.</param>
+        public virtual void BeforeTestHook(TestExecutionContext context)
+        {
+            // Just to verify our logic for detecing overridden methods works correctly.
+            // This method should never be called.
+            throw new NUnitException("BeforeTestHook must be overridden in a derived class to provide custom logic.");
+        }
+
+        /// <summary>
+        /// Method that is called <b>immediately after</b> the test is executed.
+        /// Override this to implement custom logic to run after the test.
+        /// </summary>
+        /// <param name="context">The current <see cref="TestExecutionContext"/> for the test.</param>
+        public virtual void AfterTestHook(TestExecutionContext context)
+        {
+            // Just to verify our logic for detecing overridden methods works correctly.
+            // This method should never be called.
+            throw new NUnitException("AfterTestHook must be overridden in a derived class to provide custom logic.");
+        }
+    }
+
+    /// <summary>
     /// Abstract base attribute class for activating execution hooks.
     /// </summary>
-    public abstract class ExecutionHookAttribute : NUnitAttribute, IWrapTestMethod, IApplyToContext
+    public abstract class ExecutionHookAttribute : ExecutionHookMethodsAttribute, IWrapTestMethod, IApplyToContext
     {
         /// <inheritdoc />
         public TestCommand Wrap(TestCommand command)
@@ -20,45 +50,23 @@ namespace NUnit.Framework
         /// <inheritdoc />
         public void ApplyToContext(TestExecutionContext context)
         {
-            if (IsHookImplemented(nameof(BeforeTestHook)))
+            if (BeforeTestHook != base.BeforeTestHook)
             {
+                // Only add the BeforeTestHook if it has been overridden
                 context.ExecutionHooks.AddBeforeTestHandler((sender, eventArgs) =>
                 {
                     BeforeTestHook(context);
                 });
             }
 
-            if (IsHookImplemented(nameof(AfterTestHook)))
+            if (AfterTestHook != base.AfterTestHook)
             {
+                // Only add the AfterTestHook if it has been overridden
                 context.ExecutionHooks.AddAfterTestHandler((sender, eventArgs) =>
                 {
                     AfterTestHook(context);
                 });
             }
-        }
-
-        private bool IsHookImplemented(string methodName)
-        {
-            // using a null suppression operator as we know the method exist
-            return GetType().GetMethod(methodName)!.DeclaringType != typeof(ExecutionHookAttribute);
-        }
-
-        /// <summary>
-        /// Method that is called <b>immediately before</b> the test is executed.
-        /// Override this to implement custom logic to run before the test.
-        /// </summary>
-        /// <param name="context">The current <see cref="TestExecutionContext"/> for the test.</param>
-        public virtual void BeforeTestHook(TestExecutionContext context)
-        {
-        }
-
-        /// <summary>
-        /// Method that is called <b>immediately after</b> the test is executed.
-        /// Override this to implement custom logic to run after the test.
-        /// </summary>
-        /// <param name="context">The current <see cref="TestExecutionContext"/> for the test.</param>
-        public virtual void AfterTestHook(TestExecutionContext context)
-        {
         }
     }
 }
