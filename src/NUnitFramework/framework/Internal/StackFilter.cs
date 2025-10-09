e0b0840633eb2f094b5e96182856d9f27c390a5f
Zahran Yahia Khan <114562123+zahran001@users.noreply.github.com>
2025-10-09 14:21:24 -0400

    don't change the StackFilter

--- src/NUnitFramework/framework/Internal/StackFilter.cs
+++ src/NUnitFramework/framework/Internal/StackFilter.cs
@@ -14,7 +14,7 @@ namespace NUnit.Framework.Internal
     public class StackFilter
     {
         private const string DEFAULT_TOP_OF_STACK_PATTERN = @" NUnit\.Framework\.(Assert|Assume|Warn|CollectionAssert|StringAssert|FileAssert|DirectoryAssert)\.";
-        private const string DEFAULT_BOTTOM_OF_STACK_PATTERN = @" System\.(Reflection|RuntimeMethodHandle|Threading)\.";
+        private const string DEFAULT_BOTTOM_OF_STACK_PATTERN = @" System\.(Reflection|RuntimeMethodHandle|Threading\.ExecutionContext)\.";
 
         /// <summary>
         /// Single instance of our default filter
