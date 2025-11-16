a96a6c35813ade79d9bc18c17e262bfa49d57cd5
Manfred Brands <manfred-brands@users.noreply.github.com>
2025-11-16 09:36:28 +0800

    Fix build

--- src/NUnitFramework/nunit.framework.legacy.tests/ClassicAssertExtensions_NumericTests.cs
+++ src/NUnitFramework/nunit.framework.legacy.tests/ClassicAssertExtensions_NumericTests.cs
@@ -1,6 +1,5 @@
 // Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
 
-
 namespace NUnit.Framework.Legacy.Tests
 {
     public class ClassicAssertExtensions_NumericTests
