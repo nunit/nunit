dd750f6390fe5a5bf81dd329b42be5c974c80233
Terje Sandstrom <terje@hermit.no>
2025-12-06 10:35:45 +0100

    Update src/NUnitFramework/framework/Internal/Randomizer.cs
    Co-authored-by: Steven Weerdenburg <stevenaw@users.noreply.github.com>

--- src/NUnitFramework/framework/Internal/Randomizer.cs
+++ src/NUnitFramework/framework/Internal/Randomizer.cs
@@ -500,7 +500,7 @@ namespace NUnit.Framework.Internal
         /// Returns a random string of the specified length using characters from the specified set.
         /// </summary>
         /// <param name="outputLength">The desired length of the string in char units.</param>
-        /// <param name="allowedChars">The characters to use in building the string</param>
+        /// <param name="allowedChars">The characters to use in building the string.</param>
         /// <returns>A random string.</returns>
         /// <exception cref="ArgumentException">
         /// Thrown if the requested outputLength cannot be satisfied with the provided characters
