94a2c4629423076d091a64f735a2b04748937c35
Manfred Brands <manfred-brands@users.noreply.github.com>
2025-12-11 08:27:43 +0800

    Clarify tests

--- src/NUnitFramework/tests/Constraints/AssignableFromConstraintTests.cs
+++ src/NUnitFramework/tests/Constraints/AssignableFromConstraintTests.cs
@@ -1,5 +1,6 @@
 // Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
 
+using System;
 using NUnit.Framework.Constraints;
 using static NUnit.Framework.Tests.Constraints.AssignableTestScenarios;
 
@@ -25,5 +26,34 @@ namespace NUnit.Framework.Tests.Constraints
             private static readonly object[] SuccessData = [new TTo(), new TFrom(), new object()];
             private static readonly object[] FailureData = [new TestCaseData(new B(), "<" + typeof(B).FullName + ">")];
         }
+
+        [TestCaseSource(nameof(SuccessCases))]
+        public static void CanAssignFrom(object? actual, Type type)
+        {
+            Assert.That(actual, Is.AssignableFrom(type));
+        }
+
+        private static readonly TestCaseData[] SuccessCases =
+        [
+            new TestCaseData(new object(), null),
+            new TestCaseData(42.0, typeof(int)),
+            new TestCaseData(42.0, typeof(double)),
+            new TestCaseData(42.0, typeof(float)),
+            new TestCaseData(new D1(), typeof(D2)),
+            new TestCaseData(new D1(), typeof(D3)),
+        ];
+
+        [TestCaseSource(nameof(FailureCases))]
+        public static void CanNotAssignFrom(object? actual, Type type)
+        {
+            Assert.That(actual, Is.Not.AssignableFrom(type));
+        }
+
+        private static readonly TestCaseData[] FailureCases =
+        [
+            new TestCaseData(42.0f, typeof(double)),
+            new TestCaseData(new D2(), typeof(D1)),
+            new TestCaseData(new D3(), typeof(D1)),
+        ];
     }
 }
--- src/NUnitFramework/tests/Constraints/AssignableToConstraintTests.cs
+++ src/NUnitFramework/tests/Constraints/AssignableToConstraintTests.cs
@@ -1,5 +1,6 @@
 // Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
 
+using System;
 using NUnit.Framework.Constraints;
 using static NUnit.Framework.Tests.Constraints.AssignableTestScenarios;
 
@@ -25,5 +26,37 @@ namespace NUnit.Framework.Tests.Constraints
             private static readonly object[] SuccessData = [new TTo(), new TFrom()];
             private static readonly object[] FailureData = [new TestCaseData(new object(), "<" + typeof(object).FullName + ">")];
         }
+
+        [TestCaseSource(nameof(SuccessCases))]
+        public static void CanAssignTo(object? actual, Type type)
+        {
+            Assert.That(actual, Is.AssignableTo(type));
+        }
+
+        private static readonly TestCaseData[] SuccessCases =
+        [
+            new TestCaseData(null, typeof(object)),
+            new TestCaseData(null, typeof(int?)),
+            new TestCaseData(42, typeof(int?)),
+            new TestCaseData(42, typeof(double)),
+            new TestCaseData(42, typeof(double?)),
+            new TestCaseData(42.0f, typeof(double)),
+            new TestCaseData(new D2(), typeof(D1)),
+            new TestCaseData(new D3(), typeof(D1)),
+        ];
+
+        [TestCaseSource(nameof(FailureCases))]
+        public static void CanNotAssignTo(object? actual, Type type)
+        {
+            Assert.That(actual, Is.Not.AssignableTo(type));
+        }
+
+        private static readonly TestCaseData[] FailureCases =
+        [
+            new TestCaseData(null, typeof(int)),
+            new TestCaseData(42.0, typeof(float)),
+            new TestCaseData(new D1(), typeof(D2)),
+            new TestCaseData(new D1(), typeof(D3)),
+        ];
     }
 }
