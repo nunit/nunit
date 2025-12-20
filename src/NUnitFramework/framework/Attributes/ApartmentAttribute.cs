04deabeca8d651f6aedf85c5ba7694766e2eb5bd
ARITRIK GHOSH <109242925+aritrikg@users.noreply.github.com>
2025-12-20 15:31:50 +0530

    Update ApartmentAttribute.cs

--- src/NUnitFramework/framework/Attributes/ApartmentAttribute.cs
+++ src/NUnitFramework/framework/Attributes/ApartmentAttribute.cs
@@ -1,26 +1,24 @@
-// Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
-
 using System;
 using System.Threading;
 using NUnit.Framework.Internal;
 
 namespace NUnit.Framework
 {
-    /// <summary>
-    /// Marks a test as needing to be run in a particular threading apartment state. This will cause it
-    /// to run in a separate thread if necessary.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Assembly, AllowMultiple = false, Inherited = true)]
-    public class ApartmentAttribute : PropertyAttribute
+    [AttributeUsage(
+        AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Assembly,
+        AllowMultiple = false,
+        Inherited = true)]
+    public sealed class ApartmentAttribute : PropertyAttribute
     {
-        /// <summary>
-        /// Construct an ApartmentAttribute
-        /// </summary>
-        /// <param name="apartmentState">The apartment state that this test must be run under. You must pass in a valid apartment state.</param>
         public ApartmentAttribute(ApartmentState apartmentState)
         {
-            Guard.ArgumentValid(apartmentState != ApartmentState.Unknown, "must be STA or MTA", nameof(apartmentState));
+            if (apartmentState == ApartmentState.Unknown)
+                throw new ArgumentOutOfRangeException(
+                    nameof(apartmentState),
+                    "ApartmentState must be STA or MTA");
+
             Properties.Add(PropertyNames.ApartmentState, apartmentState);
         }
     }
 }
+
