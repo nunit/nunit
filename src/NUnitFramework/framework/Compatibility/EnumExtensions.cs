82e140ae89541f2ebfb588b3c2e235a0479865da
dev-shaig <shaigfm-apa101@code.edu.az>
2026-01-07 19:17:06 +0400

    fix: Fix: Update namespace to System, rename to EnumExtensions and fix SA1127

--- src/NUnitFramework/framework/Compatibility/EnumExtensions.cs
+++ src/NUnitFramework/framework/Compatibility/EnumExtensions.cs
@@ -1,38 +1,37 @@
 // Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
 
-using System;
-
-namespace NUnit.Compatibility
+namespace System
 {
 #if NETFRAMEWORK
     /// <summary>
-    /// Provides a polyfill for the generic Enum.Parse{T} method which is available in 
+    /// Provides a polyfill for the generic Enum.Parse{T} method which is available in.
     /// .NET Core and newer .NET versions but missing in .NET Framework.
     /// </summary>
-    internal static class Enum
+    internal static class EnumExtensions
     {
         /// <summary>
-        /// Converts the string representation of the name or numeric value of one or more 
+        /// Converts the string representation of the name or numeric value of one or more.
         /// enumerated constants to an equivalent enumerated object.
         /// </summary>
         /// <typeparam name="T">The enum type to which to convert.</typeparam>
         /// <param name="value">The string representation of the enumeration name or underlying value.</param>
         /// <returns>An object of type T whose value is represented by value.</returns>
-        public static T Parse<T>(string value) where T : struct
+        public static T Parse<T>(string value)
+            where T : struct
         {
             return (T)System.Enum.Parse(typeof(T), value);
         }
-
         /// <summary>
-        /// Converts the string representation of the name or numeric value of one or more 
-        /// enumerated constants to an equivalent enumerated object. A parameter specifies 
+        /// Converts the string representation of the name or numeric value of one or more.
+        /// enumerated constants to an equivalent enumerated object. A parameter specifies.
         /// whether the operation is case-insensitive.
         /// </summary>
         /// <typeparam name="T">The enum type to which to convert.</typeparam>
         /// <param name="value">The string representation of the enumeration name or underlying value.</param>
         /// <param name="ignoreCase">true to ignore case; false to regard case.</param>
         /// <returns>An object of type T whose value is represented by value.</returns>
-        public static T Parse<T>(string value, bool ignoreCase) where T : struct
+        public static T Parse<T>(string value, bool ignoreCase)
+            where T : struct
         {
             return (T)System.Enum.Parse(typeof(T), value, ignoreCase);
         }
