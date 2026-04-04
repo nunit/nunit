d717baeec7baff2224e4eeee2176d07b482d7423
Manfred Brands <manfred-brands@users.noreply.github.com>
2026-04-04 16:46:09 +0800

    fix other flaky windows tests

--- src/NUnitFramework/tests/Assertions/AssertThatTests.cs
+++ src/NUnitFramework/tests/Assertions/AssertThatTests.cs
@@ -496,11 +496,9 @@ namespace NUnit.Framework.Tests.Assertions
         [Test]
         public async Task AssertThatWithInvalidTolerance()
         {
-            DateTime expected = DateTime.UtcNow;
-
-            await Task.Delay(500);
-
-            DateTime actual = DateTime.UtcNow;
+            var start = DateTime.UtcNow;
+            DateTime expected = start;
+            DateTime actual = start.AddMilliseconds(500);
 
             Assert.That(actual, Is.EqualTo(expected).Within(1).Seconds);
             Assert.That(() => Assert.That(actual, Is.EqualTo((object)expected).Within(1)),
@@ -522,9 +520,9 @@ namespace NUnit.Framework.Tests.Assertions
         [Test]
         public async Task AssertPropertiesComparerOnlyUsesToleranceWhereSpecified()
         {
-            var expected = new RecordWithDifferentToleranceAwareMembers(1, "Name", 1.80, DateTimeOffset.UtcNow);
-            await Task.Delay(500);
-            var actual = new RecordWithDifferentToleranceAwareMembers(2, "Name", 1.81, DateTimeOffset.UtcNow);
+            var start = DateTimeOffset.UtcNow;
+            var expected = new RecordWithDifferentToleranceAwareMembers(1, "Name", 1.80, start);
+            var actual = new RecordWithDifferentToleranceAwareMembers(2, "Name", 1.81, start.AddMilliseconds(500));
 
             // Fails because of Id and Height field
             Assert.That(actual, Is.Not.EqualTo(expected).UsingPropertiesComparer(
