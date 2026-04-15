aba3162af3dc218d60c02b0c3f3fbe67c7a18f09
stevenaw <sweerdenburg@gmail.com>
2026-04-15 12:51:08 -0400

    Update the collectiontally tests to target the generic version

--- src/NUnitFramework/tests/Internal/CollectionTallyTests.cs
+++ src/NUnitFramework/tests/Internal/CollectionTallyTests.cs
@@ -1,87 +1,106 @@
 // Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
 
+using System;
 using System.Collections.Generic;
 using NUnit.Framework.Constraints;
+using NUnit.Framework.Internal;
 
 namespace NUnit.Framework.Tests.Internal
 {
-    [TestFixture]
+    [TestFixtureSource(nameof(GetCollectionTallyTestCases))]
     public class CollectionTallyTests
     {
-        private readonly List<string> _testStrings = new List<string> { "one", "two", "three" };
+        private static readonly List<string> TestStrings = new List<string> { "one", "two", "three" };
 
-        private CollectionTally _collectionTally;
+        private readonly Func<CollectionTally<string>> _generator;
 
-        [SetUp]
-        public void TestSetup()
+        private static IEnumerable<TestFixtureParameters> GetCollectionTallyTestCases()
         {
-            _collectionTally = new CollectionTally(new NUnitEqualityComparer(), _testStrings);
+            yield return new TestFixtureParameters(() => new CollectionTally<string>(TestStrings))
+            {
+                ArgDisplayNames = ["DefaultEqualityComparer"]
+            };
+
+            yield return new TestFixtureParameters(() => new CollectionTally<string>(TestStrings, new NUnitEqualityComparer()))
+            {
+                ArgDisplayNames = ["NUnitEqualityComparer"]
+            };
+        }
+
+        public CollectionTallyTests(Func<CollectionTally<string>> generator)
+        {
+            _generator = generator;
         }
 
         [Test]
         public void TestSingularTryRemove()
         {
-            List<string> strings = new List<string>(_testStrings);
+            var collectionTally = _generator();
+            List<string> strings = new List<string>(TestStrings);
 
-            Assert.That(_collectionTally.Result.MissingItems, Has.Count.EqualTo(3));
-            Assert.That(_collectionTally.Result.ExtraItems, Is.Empty);
+            Assert.That(collectionTally.Result.MissingItems, Has.Count.EqualTo(3));
+            Assert.That(collectionTally.Result.ExtraItems, Is.Empty);
 
-            _collectionTally.TryRemove((object)strings[0]);
-            Assert.That(_collectionTally.Result.MissingItems, Has.Count.EqualTo(2));
-            Assert.That(_collectionTally.Result.ExtraItems, Is.Empty);
+            collectionTally.TryRemove(strings[0]);
+            Assert.That(collectionTally.Result.MissingItems, Has.Count.EqualTo(2));
+            Assert.That(collectionTally.Result.ExtraItems, Is.Empty);
 
-            _collectionTally.TryRemove((object)strings[1]);
-            Assert.That(_collectionTally.Result.MissingItems, Has.Count.EqualTo(1));
-            Assert.That(_collectionTally.Result.ExtraItems, Is.Empty);
+            collectionTally.TryRemove(strings[1]);
+            Assert.That(collectionTally.Result.MissingItems, Has.Count.EqualTo(1));
+            Assert.That(collectionTally.Result.ExtraItems, Is.Empty);
 
-            _collectionTally.TryRemove((object)strings[2]);
-            Assert.That(_collectionTally.Result.MissingItems, Is.Empty);
-            Assert.That(_collectionTally.Result.ExtraItems, Is.Empty);
+            collectionTally.TryRemove(strings[2]);
+            Assert.That(collectionTally.Result.MissingItems, Is.Empty);
+            Assert.That(collectionTally.Result.ExtraItems, Is.Empty);
         }
 
         [Test]
         public void TestRemoveEntireCollection()
         {
-            List<string> strings = new List<string>(_testStrings);
+            var collectionTally = _generator();
+            List<string> strings = new List<string>(TestStrings);
 
-            _collectionTally.TryRemove(strings);
-            Assert.That(_collectionTally.Result.MissingItems, Is.Empty);
-            Assert.That(_collectionTally.Result.ExtraItems, Is.Empty);
+            collectionTally.TryRemove(strings);
+            Assert.That(collectionTally.Result.MissingItems, Is.Empty);
+            Assert.That(collectionTally.Result.ExtraItems, Is.Empty);
         }
 
         [Test]
         public void TestRemoveNonExistingSingularElement()
         {
-            _collectionTally.TryRemove((object)"notFound");
+            var collectionTally = _generator();
+            collectionTally.TryRemove("notFound");
 
-            Assert.That(_collectionTally.Result.MissingItems, Has.Count.EqualTo(3));
-            Assert.That(_collectionTally.Result.ExtraItems, Has.Count.EqualTo(1));
-            Assert.That(_collectionTally.Result.ExtraItems.Contains("notFound"), Is.True);
+            Assert.That(collectionTally.Result.MissingItems, Has.Count.EqualTo(3));
+            Assert.That(collectionTally.Result.ExtraItems, Has.Count.EqualTo(1));
+            Assert.That(collectionTally.Result.ExtraItems.Contains("notFound"), Is.True);
         }
 
         [Test]
         public void TestRemoveMultipleNonExistingElements()
         {
+            var collectionTally = _generator();
             List<string> nonExistingElems = new List<string>() { "notFound", "notFound2" };
 
-            _collectionTally.TryRemove(nonExistingElems);
+            collectionTally.TryRemove(nonExistingElems);
 
-            Assert.That(_collectionTally.Result.MissingItems, Has.Count.EqualTo(3));
-            Assert.That(_collectionTally.Result.ExtraItems, Has.Count.EqualTo(2));
-            Assert.That(_collectionTally.Result.ExtraItems.Contains("notFound"), Is.True);
-            Assert.That(_collectionTally.Result.ExtraItems.Contains("notFound2"), Is.True);
+            Assert.That(collectionTally.Result.MissingItems, Has.Count.EqualTo(3));
+            Assert.That(collectionTally.Result.ExtraItems, Has.Count.EqualTo(2));
+            Assert.That(collectionTally.Result.ExtraItems.Contains("notFound"), Is.True);
+            Assert.That(collectionTally.Result.ExtraItems.Contains("notFound2"), Is.True);
         }
 
         [Test]
         public void TestRemoveSomeExistingElements()
         {
+            var collectionTally = _generator();
             List<string> someExistingElems = new List<string>() { "one", "notFound2" };
 
-            _collectionTally.TryRemove(someExistingElems);
+            collectionTally.TryRemove(someExistingElems);
 
-            Assert.That(_collectionTally.Result.MissingItems, Has.Count.EqualTo(2));
-            Assert.That(_collectionTally.Result.ExtraItems, Has.Count.EqualTo(1));
-            Assert.That(_collectionTally.Result.ExtraItems.Contains("notFound2"), Is.True);
+            Assert.That(collectionTally.Result.MissingItems, Has.Count.EqualTo(2));
+            Assert.That(collectionTally.Result.ExtraItems, Has.Count.EqualTo(1));
+            Assert.That(collectionTally.Result.ExtraItems.Contains("notFound2"), Is.True);
         }
     }
 }
